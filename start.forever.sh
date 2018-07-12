10a58558f98dce26b2f8877e399f420985314a47
franck <franck@diabeloop.fr>
2018-07-12 14:35:19 +0200

    add service name

--- start.forever.sh
+++ start.forever.sh
@@ -3,10 +3,12 @@
 . "${NVM_DIR}/nvm.sh"
 . version.sh
 
+service=blip
+
 nvm ls "${START_NODE_VERSION}" > /dev/null || { echo "ERROR: Node version ${START_NODE_VERSION} not installed"; exit 1; }
 nvm use --delete-prefix "${START_NODE_VERSION}"
 
 . config/env.sh
 
 npm run build-config
-forever -o ../server.log -e ../error.log -l ../forever.blip.log --uid blip start server.js
+forever -o ../server.log -e ../error.log -l ../forever.$service.log --uid $service start server.js
