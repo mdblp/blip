b7ebc7f90dd4057ec9363bf948235d1bf5b3a7f0
Yvon TANGUY <yvon@sentinellesduweb.com>
2019-02-12 11:49:11 +0100

    PT-111 Change e-mail & password can be denied only for patients

--- config/local.sh
+++ config/local.sh
@@ -1,7 +1,7 @@
 export API_HOST='http://localhost:8009'
 export I18N_ENABLED=false
-export ALLOW_CHANGE_EMAIL=true
-export ALLOW_CHANGE_PASSWORD=true
+export ALLOW_PATIENT_CHANGE_EMAIL=true
+export ALLOW_PATIENT_CHANGE_PASSWORD=true
 export HIDE_UPLOAD_LINK=false
 export HIDE_DEXCOM_BANNER=false
 export HIDE_DONATE=false
