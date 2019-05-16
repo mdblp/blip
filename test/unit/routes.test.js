5928205c9dc0b6c1f0fbd6a0ee278b0e200d7275
franck <franck@diabeloop.fr>
2019-05-16 16:15:49 +0200

    fixing tests

--- test/unit/routes.test.js
+++ test/unit/routes.test.js
@@ -38,7 +38,7 @@ describe('routes', () => {
 
       requiresChrome(utils, next)(nextState, replace, cb);
 
-      expect(utils.isChrome.callCount).to.equal(1);
+      expect(utils.isAcceptedBrowser.callCount).to.equal(1);
       expect(replace.callCount).to.equal(0);
       expect(cb.callCount).to.equal(0);
       expect(next.withArgs(nextState, replace, cb).callCount).to.equal(1);
@@ -56,7 +56,7 @@ describe('routes', () => {
 
       requiresChrome(utils, next)(nextState, replace, cb);
 
-      expect(utils.isChrome.callCount).to.equal(1);
+      expect(utils.isAcceptedBrowser.callCount).to.equal(1);
       expect(replace.withArgs('/browser-warning').callCount).to.equal(1);
       expect(cb.callCount).to.equal(1);
       expect(next.callCount).to.equal(0);
