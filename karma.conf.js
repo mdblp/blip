c4740bef297e01c631e0a234595c7266f7c72945
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2019-08-20 16:45:39 +0000

    Fix running unit tests

--- karma.conf.js
+++ karma.conf.js
@@ -7,12 +7,6 @@ const mochaConf = optional('./config/mocha.opts.json') || {};
 
 const testWebpackConf = _.assign({}, webpackConf, {
   devtool: 'inline-source-map',
-  plugins: [
-    new webpack.DefinePlugin({
-      __DEV__: false,
-      __TEST__: true,
-    }),
-  ],
 });
 
 delete testWebpackConf.devServer;
@@ -20,8 +14,6 @@ delete testWebpackConf.devServer;
 testWebpackConf.output = {
   filename: '[name]',
 };
-
-testWebpackConf.mode = 'development';
 
 module.exports = function karmaConfig(config) {
   config.set({
