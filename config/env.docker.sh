e326e51e1bf67873b6645a9112ece6523c22e57f
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2020-04-21 14:02:20 +0000

    PT-1254 Disable Highwater from Blip

--- config/env.docker.sh
+++ config/env.docker.sh
@@ -1,12 +1,13 @@
 ## external
 export API_HOST=http://localhost:8009
 export DEV_TOOLS=false
+export SKIP_HAKKEN='false'
 export DISCOVERY_HOST=localhost:8000
-export NODE_ENV=production
+export NODE_ENV='production'
 export PORT=3000
-export PUBLISH_HOST=hakken
-export WEBPACK_DEVTOOL=cheap-module-eval-source-map
-export SERVICE_NAME=blip
+export PUBLISH_HOST='hakken'
+export WEBPACK_DEVTOOL='cheap-module-eval-source-map'
+export SERVICE_NAME='blip'
 
 #Diabeloop specific
 export ALLOW_CHANGE_EMAIL=false
@@ -21,12 +22,14 @@ export BRANDING='diabeloop'
 export PASSWORD_MIN_LENGTH=10
 export PASSWORD_MAX_LENGTH=72
 # url to HELP system
-export HELP_LINK='disable'
+export HELP_LINK='disabled'
 # url to web server hosting the asset files
 # such as terms of use, data privacy, etc.
 export ASSETS_URL='https://url.com'
-# Matomo tracking URL (use disable to desactivate tracking)
-export MATOMO_TRACKER_URL='disable'
+# Metrics service to use (disabled, highwater, matomo)
+export METRICS_SERVICE='disabled'
+# Metrics service Matomo URL (used only when METRICS_SERVICE='matomo'):
+export MATOMO_TRACKER_URL='disabled'
 export MAX_FAILED_LOGIN_ATTEMPTS='5'
 export DELAY_BEFORE_NEXT_LOGIN_ATTEMPT='1'
 export TERMS_PRIVACY_DATE='2020-02-26'
