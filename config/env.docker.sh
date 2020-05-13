64530c653f90dc1b209796a9789a1dfd2b228ea7
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2020-05-13 14:41:19 +0000

    env declare var consistency.

--- config/env.docker.sh
+++ config/env.docker.sh
@@ -1,26 +1,26 @@
 ## external
-export API_HOST=http://localhost:8009
-export DEV_TOOLS=false
+export API_HOST='http://localhost:8009'
+export DEV_TOOLS='false'
 export SKIP_HAKKEN='false'
-export DISCOVERY_HOST=localhost:8000
+export DISCOVERY_HOST='localhost:8000'
 export NODE_ENV='production'
-export PORT=3000
+export PORT='3000'
 export PUBLISH_HOST='hakken'
 export WEBPACK_DEVTOOL='cheap-module-eval-source-map'
 export SERVICE_NAME='blip'
 
 #Diabeloop specific
-export ALLOW_CHANGE_EMAIL=false
-export I18N_ENABLED=true
-export ALLOW_CHANGE_PASSWORD=false
-export ALLOW_CHANGE_LANG=false
-export ALLOW_SIGNUP_PATIENT=false
-export HIDE_DONATE=true
-export HIDE_DEXCOM_BANNER=true
-export HIDE_UPLOAD_LINK=true
+export ALLOW_CHANGE_EMAIL='false'
+export I18N_ENABLED='true'
+export ALLOW_CHANGE_PASSWORD='false'
+export ALLOW_CHANGE_LANG='false'
+export ALLOW_SIGNUP_PATIENT='false'
+export HIDE_DONATE='true'
+export HIDE_DEXCOM_BANNER='true'
+export HIDE_UPLOAD_LINK='true'
 export BRANDING='diabeloop'
-export PASSWORD_MIN_LENGTH=10
-export PASSWORD_MAX_LENGTH=72
+export PASSWORD_MIN_LENGTH='10'
+export PASSWORD_MAX_LENGTH='72'
 # url to HELP system
 export HELP_LINK='disabled'
 # url to web server hosting the asset files
--- config/local.sh
+++ config/local.sh
@@ -1,15 +1,15 @@
 export API_HOST='http://localhost:8009'
-export I18N_ENABLED=false
+export I18N_ENABLED='false'
 export SKIP_HAKKEN='true'
-export ALLOW_SIGNUP_PATIENT=true
-export ALLOW_PATIENT_CHANGE_EMAIL=true
-export ALLOW_PATIENT_CHANGE_PASSWORD=true
-export CAN_SEE_PWD_LOGIN=true
+export ALLOW_SIGNUP_PATIENT='true'
+export ALLOW_PATIENT_CHANGE_EMAIL='true'
+export ALLOW_PATIENT_CHANGE_PASSWORD='true'
+export CAN_SEE_PWD_LOGIN='true'
 export SUPPORT_EMAIL_ADDRESS='yourloops@diabeloop.com'
 export SUPPORT_WEB_ADDRESS='https://support.diabeloop.com/hc/categories/360001386093-YourLoops'
-export HIDE_UPLOAD_LINK=false
-export HIDE_DEXCOM_BANNER=false
-export HIDE_DONATE=false
+export HIDE_UPLOAD_LINK='false'
+export HIDE_DEXCOM_BANNER='false'
+export HIDE_DONATE='false'
 export BRANDING='tidepool'
 export SERVICE_NAME='blip'
 # url to HELP system
