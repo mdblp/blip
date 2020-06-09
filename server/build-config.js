31f35fc44a61eed71968a8bde49309ac4da40d25
Yvon Tanguy <yvon.tanguy@diabeloop.fr>
2020-06-09 11:30:05 +0200

    Update server config

--- server/build-config.js
+++ server/build-config.js
@@ -2,7 +2,6 @@ const shell = require('shelljs');
 const { ShellString } = require('shelljs');
 const fs = require('fs');
 const crypto = require('crypto');
-const ms = require('ms');
 const _ = require('lodash');
 
 const reTitle = /<title>([^<]*)<\/title>/;
@@ -11,7 +10,8 @@ const reZendesk = /(<!-- Zendesk disabled -->)|(<script id="ze-snippet" type="te
 const zendeskDisable = '<!-- Zendesk disabled -->';
 const reTrackerUrl = /const u = '(.*)';/;
 const reTrackerSiteId = /const id = ([0-9]);/;
-const reCrowdin = /<!-- Crowdin Start -->([\s\S]*)<!-- Crowdin End -->/m;
+const reMatomoJs = /<!-- Start of Tracker Code -->(.*)<!-- End of Tracker Code -->/m;
+const reCrowdin = /<!-- Crowdin Start -->(.*)<!-- Crowdin End -->/m;
 
 const start = Date.now();
 
@@ -21,20 +21,19 @@ function getHash(str) {
 	return hash.digest('hex').substr(0, 20);
 }
 
-console.log('Building config...');
-if (fs.existsSync('config.app.js')) {
-  shell.exec('node config.app.js').to('dist/config.js');
-} else if (fs.existsSync('server/config.app.js')) {
-  shell.exec('node server/config.app.js').to('dist/config.js');
+if (!fs.existsSync('config.app.js')) {
+  console.error('Missing file config.app.js');
+  process.exit(1);
 }
 
-const configHash = getHash(fs.readFileSync('dist/config.js'));
-const filename = `config.${configHash}.js`;
-console.log(`Renaming "config.js" to "${filename}"...`);
-shell.mv('dist/config.js', `dist/${filename}`);
-
-console.log('Updating "dist/index.html"...');
 let indexHtml = fs.readFileSync('dist/index.html', 'utf8');
+
+console.log('Building config...');
+let configJs = shell.exec('node config.app.js');
+
+let fileHash = getHash(configJs.toString());
+const configFilename = `config.${fileHash}.js`;
+configJs.to(`dist/${configFilename}`);
 
 // Replace the title
 if (typeof process.env.BRANDING === 'string') {
@@ -44,7 +43,7 @@ if (typeof process.env.BRANDING === 'string') {
 // Replace from config.js part
 if (reConfig.test(indexHtml)) {
   const configStrOrig = reConfig.exec(indexHtml)[0];
-  const configStrRepl = `<script type="text/javascript" src="${filename}"></script>`;
+  const configStrRepl = `<script type="text/javascript" src="${configFilename}"></script>`;
   console.log(`Replace ${configStrOrig} by ${configStrRepl}`);
   indexHtml = indexHtml.replace(reConfig, configStrRepl);
 } else {
@@ -63,13 +62,13 @@ if (typeof process.env.HELP_LINK === 'string') {
   }
 }
 
-let matomoJs = '/* MaToMo tracker is disabled */';
+let matomoJs = null;
 switch (_.get(process, 'env.METRICS_SERVICE', 'disabled')) {
 case 'matomo':
   console.info('Using matomo tracker code');
   if (!_.isEmpty(process.env.MATOMO_TRACKER_URL) && process.env.MATOMO_TRACKER_URL.startsWith('http')) {
     // Replace tracker Javascript
-    matomoJs = fs.readFileSync('matomo.js', 'utf8');
+    matomoJs = fs.readFileSync('templates/matomo.js', 'utf8');
     console.info(`Setting up matomo tracker code: ${process.env.MATOMO_TRACKER_URL}`);
     const updatedSrc = matomoJs.replace(reTrackerUrl, (m, u) => {
       return m.replace(u, process.env.MATOMO_TRACKER_URL);
@@ -78,15 +77,23 @@ case 'matomo':
     matomoJs = updatedSrc.replace(reTrackerSiteId, (m, u) => {
       return m.replace(u, siteId);
     });
+
+    fileHash = getHash(matomoJs);
+    indexHtml = indexHtml.replace(reMatomoJs, `<script type="text/javascript" src="matomo.${fileHash}.js"></script>`);
+
+    let shellStr = new ShellString(matomoJs);
+    shellStr.to(`dist/matomo.${fileHash}.js`);
   } else {
     console.error('Invalid matomo config url, please verify your MATOMO_TRACKER_URL env variable');
   }
   break;
 case 'highwater':
+  indexHtml = indexHtml.replace(reMatomoJs, `<!-- Using highwater -->`);
   console.info('Using highwater tracker code');
   break;
 case 'disabled':
   console.info('Tracker code is disabled');
+  indexHtml = indexHtml.replace(reMatomoJs, `<!-- Tracker disabled -->`);
   break;
 default:
   console.error(`Unknown tracker ${process.env.METRICS_SERVICE}`);
@@ -101,7 +108,7 @@ if (typeof process.env.CROWDIN === 'string' && process.env.CROWDIN === 'enabled'
     _jipt.push(['project', 'yourloops']);\n\
   </script>\n\
   <script type=\"text/javascript\" src=\"//cdn.crowdin.com/jipt/jipt.js\"></script>\n";
-  console.log('enable crowdin');
+  console.log('Enable crowdin...');
   indexHtml = indexHtml.replace(reCrowdin,(m, u) => {
     return m.replace(u, script);
   });
@@ -112,10 +119,9 @@ if (typeof process.env.CROWDIN === 'string' && process.env.CROWDIN === 'enabled'
 }
 
 // Saving
-let shellStr = new ShellString(matomoJs);
-shellStr.to('dist/matomo.js');
-shellStr = new ShellString(indexHtml);
+console.log('Updating "dist/index.html"...');
+let shellStr = new ShellString(indexHtml);
 shellStr.to('dist/index.html');
 
 const end = Date.now();
-console.log(`Config built in ${ms(end - start)}`);
+console.log(`Config built in ${(end - start)}ms`);
