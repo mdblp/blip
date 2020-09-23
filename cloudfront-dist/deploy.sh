5a9930cda912b902e61e115770664a668ebc95d8
NasserSrairi <nasser.srairi@diabeloop.fr>
2020-09-23 16:04:05 +0200

    enhance deploy.sh to stop at first fail step

--- cloudfront-dist/deploy.sh
+++ cloudfront-dist/deploy.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -eu
 
 # WORK ONLY with the docker images !
-mv cloudfront-dist/static-dist/$FRONT_APP_NAME/index.html cloudfront-dist/pre-compiled-index.html
-# Require TARGET_ENVIRONMENT=test 
-npm run gen-lambda
-
-# Deploy, move to deployement dir in order to have access to the app in cdk.json 
-cd cloudfront-dist/deployment && cdk deploy --require-approval never $STACK_PREFIX_NAME-$FRONT_APP_NAME
+mv cloudfront-dist/static-dist/$FRONT_APP_NAME/index.html cloudfront-dist/pre-compiled-index.html && \
+    # Require TARGET_ENVIRONMENT=test 
+    npm run gen-lambda && \
+    # Deploy, move to deployement dir in order to have access to the app in cdk.json 
+    cd cloudfront-dist/deployment && \
+    echo "run cdk deploy --require-approval never $STACK_PREFIX_NAME-$FRONT_APP_NAME" && \
+    cdk deploy --require-approval never $STACK_PREFIX_NAME-$FRONT_APP_NAME
