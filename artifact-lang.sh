999f3be0710c1262ab2959edb060ecd7c9b598ba
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2020-11-18 14:12:59 +0000

    Fix PR review

--- artifact-lang.sh
+++ artifact-lang.sh
@@ -3,7 +3,8 @@ set -eu
 
 function retrieveLanguageParameters() {
   declare -a languages
-  languages=(de en es fr it nl)
+  languages="$(echo 'const l=require("./locales/languages.json"); console.log(Object.getOwnPropertyNames(l.resources).join(" "));' | node -)"
+  languages=($languages)
 
   # GIT_TOKEN: Token to access the private repository: see README.md
   OWNER=${GIT_OWNER:-mdblp}
@@ -18,6 +19,7 @@ function retrieveLanguageParameters() {
       if [ -f "locales/${K}/parameter.json" ]; then
         rm -v "locales/${K}/parameter.json"
       fi
+      echo "Fetching ${K}"
       curl -s -w "%{http_code}\n" --header "Authorization: token ${GIT_TOKEN}" \
         --header "Accept: application/vnd.github.v3.raw" \
         --output "locales/${K}/parameter.json" "https://api.github.com/repos/${OWNER}/${REPO}/contents/locales/${K}/parameter.json?ref=${GIT_BRANCH}"
