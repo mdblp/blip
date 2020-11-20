a88c771b92acda62c6a0b7b52bb246544402cbf0
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2020-11-20 09:49:02 +0000

    Fix metrics consents: - Check config - Check patient profile

--- templates/matomo.js
+++ templates/matomo.js
@@ -1,6 +1,7 @@
 var _paq = window._paq || [];
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['setDoNotTrack', true]);
+_paq.push(['requireConsent']);
 _paq.push(['trackPageView']);
 (function () {
     const u = '//localhost:8091/';
