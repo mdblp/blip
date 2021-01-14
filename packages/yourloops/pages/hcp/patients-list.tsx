a7f1218f4755e66e7faa6aeb3aef54981fa485e5
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2021-01-14 18:07:41 +0000

    Add others fields Add sort

--- packages/yourloops/pages/hcp/patients-list.tsx
+++ packages/yourloops/pages/hcp/patients-list.tsx
@@ -26,6 +26,7 @@ import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
@@ -36,16 +37,24 @@ import { t } from "../../lib/language";
 import apiClient from "../../lib/api";
 import { User } from "../../models/shoreline";
 
+type SortDirection = "asc" | "desc";
+type SortFields = "lastname" | "firstname";
+
 interface PatientListProps {
   patients: User[];
   flagged: string[];
+  order: SortDirection;
+  orderBy: SortFields;
   onClickPatient: (user: User) => void;
   onFlagPatient: (userId: string) => void;
+  onSortList: (field: SortFields, direction: SortDirection) => void;
 }
 
 interface PatientListPageState {
   patients: null | User[];
   flagged: string[];
+  order: SortDirection;
+  orderBy: SortFields;
 }
 
 const patientListStyle = makeStyles((/* theme_or_props */) => {
@@ -56,12 +65,15 @@ const patientListStyle = makeStyles((/* theme_or_props */) => {
     tableRow: {
       cursor: "pointer",
     },
+    tableRowHeader: {
+      fontVariant: "small-caps",
+    },
   };
 });
 
 
 function PatientsList(props: PatientListProps): JSX.Element {
-  const { patients, flagged, onClickPatient, onFlagPatient } = props;
+  const { patients, flagged, order, orderBy, onClickPatient, onFlagPatient, onSortList } = props;
   const classes = patientListStyle();
   const elems = [];
   const nPatients = patients.length;
@@ -90,18 +102,40 @@ function PatientsList(props: PatientListProps): JSX.Element {
         </TableCell>
         <TableCell id={`patients-list-row-lastname-${userId}`}>{lastName}</TableCell>
         <TableCell id={`patients-list-row-firstname-${userId}`}>{firstName}</TableCell>
+        <TableCell id={`patients-list-row-tir-${userId}`}>{t("N/A")}</TableCell>
+        <TableCell id={`patients-list-row-avg-glucose-${userId}`}>{t("N/A")}</TableCell>
+        <TableCell id={`patients-list-row-tbr-${userId}`}>{t("N/A")}</TableCell>
+        <TableCell id={`patients-list-row-upload-${userId}`}>{t("N/A")}</TableCell>
       </TableRow>
     );
   }
+
+  const createSortHandler = (property: SortFields) => {
+    return (/* event: React.MouseEvent */): void => {
+      onSortList(property, order === "asc" ? "desc" : "asc");
+    };
+  };
 
   return (
     <TableContainer component={Paper}>
       <Table className={classes.table} aria-label={t("aria-table-list-patient")} stickyHeader>
         <TableHead>
-          <TableRow>
+          <TableRow className={classes.tableRowHeader}>
             <TableCell id="patients-list-header-flag" />
-            <TableCell id="patients-list-header-lastname">{t("list-patient-lastname")}</TableCell>
-            <TableCell id="patients-list-header-firstname">{t("list-patient-firstname")}</TableCell>
+            <TableCell id="patients-list-header-lastname">
+              <TableSortLabel active={orderBy === "lastname"} direction={order} onClick={createSortHandler("lastname")}>
+                {t("list-patient-lastname")}
+              </TableSortLabel>
+            </TableCell>
+            <TableCell id="patients-list-header-firstname">
+              <TableSortLabel active={orderBy === "firstname"} direction={order} onClick={createSortHandler("firstname")}>
+                {t("list-patient-firstname")}
+              </TableSortLabel>
+            </TableCell>
+            <TableCell id="patients-list-header-tir">{t("list-patient-tir")}</TableCell>
+            <TableCell id="patients-list-header-avg-glucose">{t("list-patient-avg-glucose")}</TableCell>
+            <TableCell id="patients-list-header-tbr">{t("list-patient-tbr")}</TableCell>
+            <TableCell id="patients-list-header-upload">{t("list-patient-upload")}</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
@@ -124,10 +158,13 @@ class PatientListPage extends React.Component<RouteComponentProps, PatientListPa
     this.state = {
       patients: null,
       flagged: whoAmI?.preferences?.patientsStarred ?? [],
+      order: "asc",
+      orderBy: "lastname",
     };
 
     this.onSelectPatient = this.onSelectPatient.bind(this);
     this.onFlagPatient = this.onFlagPatient.bind(this);
+    this.onSortList = this.onSortList.bind(this);
   }
 
   public componentDidMount(): void {
@@ -141,11 +178,20 @@ class PatientListPage extends React.Component<RouteComponentProps, PatientListPa
   }
 
   render(): JSX.Element | null {
-    const { patients, flagged } = this.state;
+    const { patients, flagged, order, orderBy } = this.state;
     let listPatients: JSX.Element | null = null;
 
     if (patients !== null) {
-      listPatients = <PatientsList patients={patients} flagged={flagged} onClickPatient={this.onSelectPatient} onFlagPatient={this.onFlagPatient} />;
+      listPatients = (
+        <PatientsList
+          patients={patients}
+          flagged={flagged}
+          order={order}
+          orderBy={orderBy}
+          onClickPatient={this.onSelectPatient}
+          onFlagPatient={this.onFlagPatient}
+          onSortList={this.onSortList} />
+      );
     }
     return listPatients;
   }
@@ -159,6 +205,59 @@ class PatientListPage extends React.Component<RouteComponentProps, PatientListPa
     apiClient.flagPatient(userId).then((flagged: string[]) => {
       this.setState({ flagged });
     });
+  }
+
+  /**
+   * Compare two patient for sorting the patient table
+   * @param a A patient
+   * @param b A âtient
+   * @param flagged Pinned patient
+   * @param orderBy Sort field
+   */
+  private doCompare(a: User, b: User, orderBy: SortFields): number {
+    let aValue: string;
+    let bValue: string;
+    switch (orderBy) {
+    case "firstname":
+      aValue = a.profile?.firstName ?? "😀";
+      bValue = b.profile?.firstName ?? "😀";
+      break;
+    case "lastname":
+      aValue = a.profile?.lastName ?? a.profile?.fullName ?? a.username;
+      bValue = b.profile?.lastName ?? b.profile?.fullName ?? b.username;
+      break;
+    }
+
+    return aValue.localeCompare(bValue);
+  }
+
+  private onSortList(orderBy: SortFields, order: SortDirection): void {
+    const { patients, flagged } = this.state;
+
+    patients?.sort((a: User, b: User): number => {
+      const aFlagged = flagged.includes(a.userid);
+      const bFlagged = flagged.includes(b.userid);
+      // Flagged: always first
+      if (aFlagged && !bFlagged) {
+        return -1; // eslint-disable-line no-magic-numbers
+      }
+      if (!aFlagged && bFlagged) {
+        return 1;
+      }
+
+      let c = this.doCompare(a, b, orderBy);
+      if (c === 0) {
+        // In case of equality: choose another field
+        if (orderBy === "lastname") {
+          c = this.doCompare(a, b, "firstname");
+        } else {
+          c = this.doCompare(a, b, "lastname");
+        }
+      }
+      return order === "asc" ? c : -c;
+    });
+
+    this.setState({ patients, order, orderBy });
   }
 }
 
