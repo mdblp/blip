a4603d527517337327306fcefc937403ae03fd43
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2021-01-22 17:34:43 +0000

    Fix compile after merge

--- packages/yourloops/pages/hcp/teams-list.tsx
+++ packages/yourloops/pages/hcp/teams-list.tsx
@@ -39,7 +39,6 @@ import Button from "@material-ui/core/Button";
 import CircularProgress from "@material-ui/core/CircularProgress";
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 
