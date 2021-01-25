2191196bd7c40300506f73ea7d7508f87d76d4ef
Léo Valette <leo.valette@diabeloop.fr>
2021-01-25 16:05:51 +0100

    Remove mocked user

--- packages/yourloops/pages/profile/profile.tsx
+++ packages/yourloops/pages/profile/profile.tsx
@@ -14,6 +14,22 @@
  * not, you can obtain one from Tidepool Project at tidepool.org.
  */
 
+import {
+  AppBar,
+  Breadcrumbs,
+  Button,
+  Container,
+  FormControl,
+  FormControlLabel,
+  InputLabel,
+  Link,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+  Toolbar,
+} from '@material-ui/core';
 import React, {
   CSSProperties,
   Fragment,
@@ -23,41 +39,20 @@ import React, {
   useMemo,
   useState,
 } from 'react';
-import _ from 'lodash';
+import { Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 
-import {
-  Button,
-  TextField,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  RadioGroup,
-  FormControlLabel,
-  Radio,
-} from '@material-ui/core';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import HeaderBar from '../../components/header-bar';
-import { User } from 'models/shoreline';
-import { REGEX_EMAIL } from '../../lib/utils';
+import HomeIcon from '@material-ui/icons/Home';
 import { Password } from '../../components/utils/password';
+import { REGEX_EMAIL } from '../../lib/utils';
+import _ from 'lodash';
+import apiClient from '../../lib/auth/api';
+import { t } from '../../lib/language';
 
 enum Units {
   mole = 'mmol/L',
   gram = 'mg/dL',
 }
-
-const getMockUser = (userid: User['userid']): User => ({
-  userid,
-  username: '',
-  emails: ['ylp.ui.test+guest10@diabeloop.fr'],
-  profile: {
-    fullName: 'Léo Valette',
-    firstName: 'Léo',
-    lastName: 'Valette',
-    patient: {},
-  },
-});
 
 const availableLocalesMock = ['français', 'anglais', 'italien'];
 
@@ -69,6 +64,19 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     selectEmpty: {
       marginTop: theme.spacing(2),
+    },
+    homeIcon: {
+      marginRight: '0.5em',
+    },
+    breadcrumbLink: {
+      display: 'flex',
+    },
+    toolBar: {
+      display: 'grid',
+      gridTemplateRows: 'auto',
+      gridTemplateColumns: 'auto auto auto',
+      paddingLeft: '6em',
+      paddingRight: '6em',
     },
   })
 );
@@ -89,27 +97,28 @@ export const ProfilePage: FunctionComponent = () => {
   const [password, setPassword] = useState('');
   const [passwordConfirmation, setPasswordConfirmation] = useState('');
   const [unit, setUnit] = useState(Units.mole);
+  const [hasChanged, setHasChanged] = useState(false);
 
-  const userid = 'userid';
   const classes = useStyles();
 
   useEffect(() => {
-    const { profile, emails } = getMockUser(userid);
-    if (profile?.firstName) {
-      setFirstName(profile.firstName);
+    const user = apiClient.whoami;
+    if (user?.profile?.firstName) {
+      setFirstName(user.profile.firstName);
     }
-    if (profile?.lastName) {
-      setName(profile.lastName);
+    if (user?.profile?.lastName) {
+      setName(user.profile.lastName);
     }
-    if (emails && emails.length) {
-      setMail(emails[0]);
+    if (user?.emails && user.emails.length) {
+      setMail(user.emails[0]);
     }
-  }, [userid]);
+  }, []);
 
   const handleChange = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
     setState: React.Dispatch<React.SetStateAction<string>>
-  ) => {
+  ): void => {
+    setHasChanged(true);
     setState(event.target.value);
   };
 
@@ -118,11 +127,15 @@ export const ProfilePage: FunctionComponent = () => {
       name?: string | undefined;
       value: string | unknown;
     }>
-  ) => {
+  ): void => {
+    setHasChanged(true);
     setLocale(event.target.value as string);
   };
 
-  const handleUnitChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleUnitChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ): void => {
+    setHasChanged(true);
     if (event.target.value === Units.mole) {
       setUnit(Units.mole);
     } else if (event.target.value === Units.gram) {
@@ -135,13 +148,14 @@ export const ProfilePage: FunctionComponent = () => {
       firstName: _.isEmpty(firstName),
       name: _.isEmpty(name),
       mail: !REGEX_EMAIL.test(mail),
-      password: password.length < 10,
+      password: password.length > 0 && password.length < 10,
       passwordConfirmation: passwordConfirmation !== password,
     }),
     [firstName, name, mail, password, passwordConfirmation]
   );
 
   const onSave = useCallback(() => {
+    setHasChanged(false);
     console.log('save'); // TODO: API Call
   }, [firstName, name, mail]);
 
@@ -150,106 +164,125 @@ export const ProfilePage: FunctionComponent = () => {
   return (
     <Fragment>
       <HeaderBar />
-      <div style={{ display: 'flex', flexDirection: 'column', margin: '16px' }}>
-        <TextField
-          id='firstName'
-          label='firstName'
-          value={firstName}
-          onChange={(e) => handleChange(e, setFirstName)}
-          error={errors.firstName}
-          helperText={errors.firstName && 'Field required'}
-          variant='outlined'
-          style={textFieldStyle}
-        />
-        <TextField
-          id='lastName'
-          label='lastName'
-          value={name}
-          onChange={(e) => handleChange(e, setName)}
-          error={errors.name}
-          helperText={errors.name && 'Field required'}
-          variant='outlined'
-          style={textFieldStyle}
-        />
-        <TextField
-          id='mail'
-          label='mail'
-          value={mail}
-          onChange={(e) => handleChange(e, setMail)}
-          error={errors.mail}
-          helperText={errors.mail && 'Mail incorrect'}
-          variant='outlined'
-          style={textFieldStyle}
-        />
-        <Password
-          id='password'
-          label='password'
-          value={password}
-          error={errors.password}
-          helperText={'Password too weak'}
-          style={textFieldStyle}
-          setState={setPassword}
-        />
-        <Password
-          id='passwordConfirmation'
-          label='password confirmation'
-          value={passwordConfirmation}
-          error={errors.passwordConfirmation}
-          helperText={'Passwords are not matching'}
-          style={textFieldStyle}
-          setState={setPasswordConfirmation}
-        />
-        <FormControl variant='outlined' className={classes.formControl}>
-          <InputLabel
-            style={{ backgroundColor: '#f7f7f8', padding: ' 0 8px' }}
-            id='locale-selector'
-          >
-            Locale
-          </InputLabel>
-          <Select
-            labelId='locale-selector'
-            id='locale-selector'
-            value={locale}
-            onChange={handleLocaleChange}
-          >
-            {availableLocalesMock.map((locale) => (
-              <MenuItem key={locale} value={locale}>
-                {locale}
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <FormControl>
-          <RadioGroup
-            row
-            aria-label='position'
-            name='unit'
-            value={unit}
-            onChange={handleUnitChange}
-          >
-            <FormControlLabel
-              value={Units.mole}
-              control={<Radio color='primary' />}
-              label={Units.mole}
-              labelPlacement='start'
-            />
-            <FormControlLabel
-              value={Units.gram}
-              control={<Radio color='primary' />}
-              label={Units.gram}
-              labelPlacement='start'
-            />
-          </RadioGroup>
-        </FormControl>
-        <Button
-          variant='contained'
-          color='primary'
-          onClick={onSave}
-          style={{ margin: '8px' }}
+      <AppBar position='static' color='secondary'>
+        <Toolbar className={classes.toolBar}>
+          <Breadcrumbs aria-label={t('breadcrumb')}>
+            <Link
+              className={classes.breadcrumbLink}
+              color='textPrimary'
+              href='/hcp'
+            >
+              <HomeIcon className={classes.homeIcon} />
+              {t('My Patients')}
+            </Link>
+          </Breadcrumbs>
+        </Toolbar>
+      </AppBar>
+      <Container maxWidth='lg'>
+        <div
+          style={{ display: 'flex', flexDirection: 'column', margin: '16px' }}
         >
-          Save
-        </Button>
-      </div>
+          <TextField
+            id='firstName'
+            label='firstName'
+            value={firstName}
+            onChange={(e) => handleChange(e, setFirstName)}
+            error={errors.firstName}
+            helperText={errors.firstName && 'Field required'}
+            variant='outlined'
+            style={textFieldStyle}
+          />
+          <TextField
+            id='lastName'
+            label='lastName'
+            value={name}
+            onChange={(e) => handleChange(e, setName)}
+            error={errors.name}
+            helperText={errors.name && 'Field required'}
+            variant='outlined'
+            style={textFieldStyle}
+          />
+          <TextField
+            id='mail'
+            label='mail'
+            value={mail}
+            onChange={(e) => handleChange(e, setMail)}
+            error={errors.mail}
+            helperText={errors.mail && 'Mail incorrect'}
+            variant='outlined'
+            style={textFieldStyle}
+          />
+          <Password
+            id='password'
+            label='password'
+            value={password}
+            error={errors.password}
+            helperText={'Password too weak'}
+            style={textFieldStyle}
+            setState={setPassword}
+          />
+          <Password
+            id='passwordConfirmation'
+            label='password confirmation'
+            value={passwordConfirmation}
+            error={errors.passwordConfirmation}
+            helperText={'Passwords are not matching'}
+            style={textFieldStyle}
+            setState={setPasswordConfirmation}
+          />
+          <FormControl variant='outlined' className={classes.formControl}>
+            <InputLabel
+              style={{ backgroundColor: '#f7f7f8', padding: ' 0 8px' }}
+              id='locale-selector'
+            >
+              Locale
+            </InputLabel>
+            <Select
+              labelId='locale-selector'
+              id='locale-selector'
+              value={locale}
+              onChange={handleLocaleChange}
+            >
+              {availableLocalesMock.map((locale) => (
+                <MenuItem key={locale} value={locale}>
+                  {locale}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl>
+            <RadioGroup
+              row
+              aria-label='position'
+              name='unit'
+              value={unit}
+              onChange={handleUnitChange}
+            >
+              <FormControlLabel
+                value={Units.mole}
+                control={<Radio color='primary' />}
+                label={Units.mole}
+                labelPlacement='start'
+              />
+              <FormControlLabel
+                value={Units.gram}
+                control={<Radio color='primary' />}
+                label={Units.gram}
+                labelPlacement='start'
+              />
+            </RadioGroup>
+          </FormControl>
+          <Button
+            variant='contained'
+            disabled={!hasChanged}
+            color='primary'
+            onClick={onSave}
+            style={{ margin: '8px' }}
+          >
+            Save
+          </Button>
+        </div>
+      </Container>
     </Fragment>
   );
 };
