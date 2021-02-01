79f1360e2b8a76b8c8caffb5104a7ceb0868e9e5
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2021-02-01 09:19:00 +0000

    i18next: respect the promise

--- packages/yourloops/app/index.tsx
+++ packages/yourloops/app/index.tsx
@@ -26,18 +26,20 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-import React from "react";
+import * as React from "react";
 import ReactDOM from "react-dom";
 import Yourloops from "./app";
 
-import "../lib/language";
+import { init as i18nInit } from "../lib/language";
 
 // TODO: window.onerror
 
-let div = document.getElementById("app");
-if (div === null) {
-  div = document.createElement("div");
-  div.id = "app";
-  document.body.appendChild(div);
-}
-ReactDOM.render(<Yourloops />, div);
+i18nInit().then(() => {
+  let div = document.getElementById("app");
+  if (div === null) {
+    div = document.createElement("div");
+    div.id = "app";
+    document.body.appendChild(div);
+  }
+  ReactDOM.render(<Yourloops />, div);
+});
