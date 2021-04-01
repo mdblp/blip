32cb14c9e483b41c7611e16865a6e98881f0faa0
NasserSrairi <nasser.srairi@diabeloop.fr>
2021-04-01 16:20:16 +0200

    fix: link cpt force a full reload

--- packages/yourloops/pages/notifications/notifications-list.tsx
+++ packages/yourloops/pages/notifications/notifications-list.tsx
@@ -16,12 +16,23 @@
  */
 
 import * as React from "react";
+import { useTranslation } from "react-i18next";
+import { Link as RouterLink } from "react-router-dom";
 
 import Container from "@material-ui/core/Container";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import Link from "@material-ui/core/Link";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 
 import SecondaryHeaderBar from "./secondary-bar";
+import HomeIcon from "@material-ui/icons/Home";
+import NavigateNextIcon from "@material-ui/icons/NavigateNext";
+import HeaderBar from "../../components/header-bars/primary";
 import { Notification } from "./notification";
 import { INotification } from "../../lib/notifications/models";
 import { useAuth } from "../../lib/auth";
@@ -32,10 +43,65 @@ interface NotificationsPageProps {
   defaultURL: string;
 }
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    homeIcon: {
+      marginRight: "0.5em",
+    },
+    breadcrumbLink: {
+      display: "flex",
+    },
+    toolBar: {
+      display: "grid",
+      gridTemplateRows: "auto",
+      gridTemplateColumns: "auto auto auto",
+      paddingLeft: "6em",
+      paddingRight: "6em",
+    },
+    typography: {
+      textAlign: "center",
+      margin: theme.spacing(4),
+    },
+  })
+);
+
+const NotificationHeader = () => {
+  const { t } = useTranslation("yourloops");
+  const classes = useStyles();
+  const { user } = useAuth();
+  const homePage = `/${user?.role ?? ""}`;
+
+  return (
+    <React.Fragment>
+      <HeaderBar />
+      <AppBar position="static" color="secondary">
+        <Toolbar className={classes.toolBar}>
+          <Breadcrumbs
+            aria-label={t("aria-breadcrumbs")}
+            separator={<NavigateNextIcon fontSize="small" />}>
+            <Link
+              component={RouterLink}
+              to={homePage}
+              className={classes.breadcrumbLink}
+              color="textPrimary"
+            >
+              <HomeIcon className={classes.homeIcon} />
+              {t("home")}
+            </Link>
+            <div>{t("notifications")}</div>
+          </Breadcrumbs>
+        </Toolbar>
+      </AppBar>
+    </React.Fragment>
+  );
+};
+
 const sortNotification = (notifA: INotification, notifB: INotification): number =>
   Date.parse(notifB.created) - Date.parse(notifA.created);
 
 export const NotificationsPage = (props: NotificationsPageProps): JSX.Element => {
+  const { t } = useTranslation("yourloops");
+  const classes = useStyles();
   const { user } = useAuth();
   const notifications = useNotification();
   const [notifs, setNotifs] = React.useState<INotification[]>([]);
@@ -51,7 +117,7 @@ export const NotificationsPage = (props: NotificationsPageProps): JSX.Element =>
       } catch (reason: unknown) {
         const errorMessage = errorTextFromException(reason);
         const message = t(errorMessage);
-        console.log("toto", message);
+        console.log(message);
         //openSnackbar({ message, severity: AlertSeverity.error });
       }
     };
@@ -69,9 +135,8 @@ export const NotificationsPage = (props: NotificationsPageProps): JSX.Element =>
       <SecondaryHeaderBar defaultURL={props.defaultURL} />
       <Container maxWidth="lg" style={{ marginTop: "1em" }}>
         <List>
-          {notifs
-            .sort(sortNotification)
-            .map(({ id, created, creator, type, target }, index) => (
+          {notifs.length > 0 ? (
+            notifs.sort(sortNotification).map(({ id, created, creator, type, target }, index) => (
               <ListItem
                 key={index}
                 style={{ padding: "8px 0" }}
@@ -85,7 +150,16 @@ export const NotificationsPage = (props: NotificationsPageProps): JSX.Element =>
                   onRemove={handleRemove}
                 />
               </ListItem>
-            ))}
+            ))
+          ) : (
+            <Typography
+              className={classes.typography}
+              id="typography-no-pending-invitation-message"
+              variant="body2"
+              gutterBottom>
+              {t("no-pending-invitation-message")}
+            </Typography>
+          )}
         </List>
       </Container>
     </React.Fragment>
