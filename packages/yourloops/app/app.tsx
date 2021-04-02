ea523aa656b312bb46f660fd23c21e2c3c0bbd5e
NasserSrairi <nasser.srairi@diabeloop.fr>
2021-04-02 14:41:03 +0200

    fix: footerlinks repeated several times

--- packages/yourloops/app/app.tsx
+++ packages/yourloops/app/app.tsx
@@ -48,21 +48,21 @@ import { NotificationContextProvider } from "../lib/notifications/hook";
 const Yourloops: React.FunctionComponent = () => (
   <Router>
     <AuthContextProvider>
-      <Switch>
-        <PublicRoute exact path="/" component={LoginPage} />
-        {/* this route is required because some backend service generate URL with it */}
-        <PublicRoute exact path="/login" component={LoginPage} />
-        <PublicRoute exact path="/signup" component={SignUpPage} />
-        <PublicRoute path="/request-password-reset" component={RequestPasswordResetPage} />
-        <PublicRoute path="/confirm-password-reset" component={ConfirmPasswordResetPage} />
-        <NotificationContextProvider>
+      <NotificationContextProvider>
+        <Switch>
+          <PublicRoute exact path="/" component={LoginPage} />
+          {/* this route is required because some backend service generate URL with it */}
+          <PublicRoute exact path="/login" component={LoginPage} />
+          <PublicRoute exact path="/signup" component={SignUpPage} />
+          <PublicRoute path="/request-password-reset" component={RequestPasswordResetPage} />
+          <PublicRoute path="/confirm-password-reset" component={ConfirmPasswordResetPage} />
           <PrivateRoute path="/caregiver" component={CaregiverPage} />
           <PrivateRoute path="/hcp" component={HcpPage} />
           <PrivateRoute path="/patient" component={PatientPage} />
           <PrivateRoute path="/account-preferences" component={ProfilePage} />
           <PrivateRoute path="/notifications" component={NotificationsPage} />
-        </NotificationContextProvider>
-      </Switch>
+        </Switch>
+      </NotificationContextProvider>
     </AuthContextProvider>
   </Router>
 );
