ad45abbafccece1dfbafe55e995a2986f284c933
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2021-05-19 09:14:18 +0000

    Fix TU

--- packages/yourloops/test/pages/hcp/teams-page.test.tsx
+++ packages/yourloops/test/pages/hcp/teams-page.test.tsx
@@ -32,12 +32,14 @@ import { mount, ReactWrapper, MountRendererProps } from "enzyme";
 import sinon from "sinon";
 
 import { AuthContextProvider } from "../../../lib/auth";
+import { NotificationContextProvider } from "../../../lib/notifications";
 import { TeamContextProvider } from "../../../lib/team";
 import TeamsPage from "../../../pages/hcp/teams-page";
 
 import { waitTimeout } from "../../../lib/utils";
 import { teams } from "../../common";
 import { authHookHcp } from "../../lib/auth/hook.test";
+import { stubNotficationContextValue } from "../../lib/notifications/hook.test";
 import { teamAPI, resetTeamAPIStubs } from "../../lib/team/hook.test";
 
 function testTeamPage(): void {
@@ -48,11 +50,14 @@ function testTeamPage(): void {
   let component: ReactWrapper | null = null;
 
   function TestTeamsPageComponent(): JSX.Element {
+
     return (
       <AuthContextProvider value={authHookHcp}>
-        <TeamContextProvider api={teamAPI}>
-          <TeamsPage />
-        </TeamContextProvider>
+        <NotificationContextProvider value={stubNotficationContextValue}>
+          <TeamContextProvider api={teamAPI}>
+            <TeamsPage />
+          </TeamContextProvider>
+        </NotificationContextProvider>
       </AuthContextProvider>
     );
   }
