9d05362705de2bae6d66de5c55adaa11a7e7e4c9
Yvon TANGUY <yvon.tanguy@diabeloop.fr>
2021-06-16 09:26:59 +0000

    Fix PR review

--- packages/blip/app/core/api-utils.js
+++ packages/blip/app/core/api-utils.js
@@ -90,6 +90,7 @@ class ApiUtils {
   async refreshV1() {
     const range = await this.api.getPatientDataRange(this.patient);
     if (range === null) {
+      this.log.info("Range is empty - no data available");
       return [];
     }
     this.log.info("Available data range:", range[0], range[1]);
