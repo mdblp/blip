17cadd7d7fc16fdee73797beb70f86cefc9b3ace
Arnaud <arno.tgh@gmail.com>
2021-10-11 17:50:54 +0200

    adding test for save button

--- packages/yourloops/test/pages/profile/profile.test.tsx
+++ packages/yourloops/test/pages/profile/profile.test.tsx
@@ -27,67 +27,90 @@
  */
 
 import * as React from "react";
+import { render, unmountComponentAtNode } from 'react-dom';
+import { act, Simulate, SyntheticEventData } from 'react-dom/test-utils';
 import { BrowserRouter } from "react-router-dom";
 import { expect } from "chai";
-import { mount, ReactWrapper } from "enzyme";
 
-import { AuthContext, AuthContextProvider } from "../../../lib/auth";
-import { authHookHcp, authHookPatient } from "../../lib/auth/hook.test";
+import { Units } from "../../../models/generic";
+import { AuthContextProvider, Session } from "../../../lib/auth";
+import { loggedInUsers } from "../../common";
+import { createAuthHookStubs } from "../../lib/auth/hook.test";
 import { NotificationContextProvider } from "../../../lib/notifications";
 import { stubNotficationContextValue } from "../../lib/notifications/hook.test";
-import { Units } from "../../../models/generic";
-import { UserRoles } from "../../../models/shoreline";
-import PatientProfileForm from "../../../pages/profile/patient-form";
 import ProfilePage from "../../../pages/profile";
 
 function testProfile(): void {
-  let component: ReactWrapper | null = null;
+  let container: HTMLElement | null = null;
   const defaultUrl = "/professional/patients";
 
-  function ProfilePageTestComponent(props: AuthContext): JSX.Element {
-    return (
-      <BrowserRouter>
-        <AuthContextProvider value={props}>
-          <NotificationContextProvider value={stubNotficationContextValue}>
-            <ProfilePage defaultURL={defaultUrl} />
-          </NotificationContextProvider>
-        </AuthContextProvider>
-      </BrowserRouter>
-    );
+  function mountProfilePage(session: Session): void {
+    const context = createAuthHookStubs(session);
+    act(() => {
+      render(
+        <BrowserRouter>
+          <AuthContextProvider value={context}>
+            <NotificationContextProvider value={stubNotficationContextValue}>
+              <ProfilePage defaultURL={defaultUrl} />
+            </NotificationContextProvider>
+          </AuthContextProvider>
+        </BrowserRouter>,
+        container
+      );
+    });
   }
 
-  function mountProfilePage(authHook: AuthContext): ReactWrapper {
-    return mount(<ProfilePageTestComponent {...authHook} />);
-  }
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
 
   afterEach(() => {
-    if (component !== null) {
-      component.unmount();
-      component = null;
+    if (container) {
+      unmountComponentAtNode(container);
+      container.remove();
+      container = null;
     }
   });
 
   it("should be able to render", () => {
-    component = mountProfilePage(authHookHcp);
-    expect(component.exists("#profile-textfield-firstname")).to.be.true;
-    expect(component.exists("#profile-button-save")).to.be.true;
+    mountProfilePage(loggedInUsers.hcpSession);
+    expect(container.querySelector("#profile-textfield-firstname").id).to.be.equal("profile-textfield-firstname");
+    expect(container.querySelector("#profile-button-save").id).to.be.equal("profile-button-save");
   });
 
   it("should display mg/dL Units by default if not specified", () => {
-    const { user } = authHookHcp;
-    delete user?.settings?.units?.bg;
-    component = mountProfilePage(authHookHcp);
-    const selectValue = component.find("#profile-units-selector").first().prop("value");
+    const session = loggedInUsers.hcpSession;
+    delete session.user?.settings?.units?.bg;
+    mountProfilePage(session);
+    const selectValue = container.querySelector("#profile-units-selector").innerHTML;
     expect(selectValue).to.be.equal(Units.gram);
   });
 
   it("should display birthdate if user is a patient", () => {
-    const { user } = authHookPatient;
-    if (user?.role === UserRoles.patient) {
-      component = mountProfilePage(authHookPatient);
-      expect(component.find(PatientProfileForm).prop("birthDate"))
-        .to.be.equal(user.profile?.patient?.birthday);
-    }
+    const session = loggedInUsers.patientSession;
+    mountProfilePage(session);
+    const birthDateInput = container.querySelector("#profile-textfield-birthdate") as HTMLInputElement;
+    expect(birthDateInput?.value).to.be.equal(session.user.profile?.patient?.birthday);
+  });
+
+  it("should enable save button when changes are made", () => {
+    const session = loggedInUsers.hcpSession;
+    mountProfilePage(session);
+
+    const saveButton: HTMLButtonElement = container.querySelector("#profile-button-save");
+    const firstnameInput: HTMLInputElement = container.querySelector("#profile-textfield-firstname");
+    const languageSelectInput = container.querySelector("#profile-locale-selector + input");
+    const unitSelectInput = container?.querySelector("#profile-units-selector + input");
+
+    expect(saveButton.disabled, "button is disabled").to.be.true;
+
+    Simulate.change(firstnameInput, { target: { value: "Chandler" } } as unknown as SyntheticEventData);
+    Simulate.change(languageSelectInput, { target: { value: "es" } } as unknown as SyntheticEventData);
+    Simulate.change(unitSelectInput, { target: { value: Units.mole } } as unknown as SyntheticEventData);
+
+    expect(saveButton.disabled, "button is enabled").to.be.false;
+    Simulate.click(saveButton);
   });
 }
 
