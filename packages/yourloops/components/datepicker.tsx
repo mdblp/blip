90bd2257ddcbd32304e3d126a3a2c000e875b510
Arnaud <arno.tgh@gmail.com>
2021-10-22 11:42:09 +0200

    better syntax for Datepicker

--- packages/yourloops/components/datepicker.tsx
+++ packages/yourloops/components/datepicker.tsx
@@ -1,3 +1,31 @@
+/**
+ * Copyright (c) 2021, Diabeloop
+ * Simple DatePicker
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import React, { FunctionComponent, useEffect, useState } from "react";
 import i18next from "i18next";
 import { useTranslation } from "react-i18next";
@@ -14,7 +42,7 @@ import { MaterialUiPickersDate } from "@material-ui/pickers/typings/date";
 
 import { getCurrentLang } from "../lib/language";
 
-interface Props {
+interface DatePickerProps {
   date: MaterialUiPickersDate;
   title: FunctionComponent;
 }
@@ -34,7 +62,7 @@ class LocalizedUtils extends DayjsUtils {
   }
 }
 
-const DatePicker: FunctionComponent<Props> = ({ date, title }: Props) => {
+function DatePicker({ date, title }: DatePickerProps): JSX.Element {
   const { t } = useTranslation("yourloops");
   const [selectedDate, handleDateChange] = useState(date as MaterialUiPickersDate);
   const [isOpen, setIsOpen] = useState(false);
@@ -44,7 +72,7 @@ const DatePicker: FunctionComponent<Props> = ({ date, title }: Props) => {
     console.log(selectedDate);
   }, [selectedDate]);
 
-  const textFieldOverride: FunctionComponent = () => (
+  const textFieldOverride = () => (
     <div
       className={`chart-title-clickable ${isOpen ? "active" : ""}`}
       onClick={() => setIsOpen(true)}
@@ -70,6 +98,6 @@ const DatePicker: FunctionComponent<Props> = ({ date, title }: Props) => {
       />
     </MuiPickersUtilsProvider>
   );
-};
+}
 
 export default DatePicker;
