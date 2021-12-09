a2fc0d5459372b7887e6e1034498e97c200df08f
Arnaud <arnaud.groby@diabeloop.fr>
2021-12-09 15:32:33 +0100

    fixing favicon and test lambda

--- test-lambda.sh
+++ test-lambda.sh
@@ -4,9 +4,11 @@ set -eu
 export TARGET_ENVIRONMENT='test'
 export API_HOST='https://api.example.com'
 
+FAVICON="$(echo 'const l=require("./branding/branding.json"); console.log(l[process.env.BRANDING].favicon)' | node -)"
+
 rm -rf 'dist'
 mkdir -pv 'dist/static'
 cp -v 'templates/index.html' 'dist/static/'
-cp -v 'branding/diabeloop/favicon.ico' 'dist/static/'
+cp -v "branding/${FAVICON}" 'dist/static/'
 npm run gen-lambda
 mocha test/lambda/cloudfront-gen-lambda.test.js
