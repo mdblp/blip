31a9372a93f257e5b86969b66409793885775eb4
Arnaud <arnaud.groby@diabeloop.fr>
2021-12-09 16:00:04 +0100

    [FIX] missing BRANDING var

--- test-lambda.sh
+++ test-lambda.sh
@@ -3,6 +3,7 @@ set -eu
 
 export TARGET_ENVIRONMENT='test'
 export API_HOST='https://api.example.com'
+export BRANDING='diabeloop/blue'
 
 FAVICON="$(echo 'const l=require("./branding/branding.json"); console.log(l[process.env.BRANDING].favicon)' | node -)"
 
