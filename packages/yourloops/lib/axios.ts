cee08d886dc096f011413776d01efb87e8021857
Arnaud <arnaud.groby@diabeloop.fr>
2022-01-20 12:14:11 +0100

    adding axios

---
+++ packages/yourloops/lib/axios.ts
@@ -0,0 +1,13 @@
+import axios, { AxiosRequestConfig } from "axios";
+import appConfig from "./config";
+
+axios.defaults.baseURL = appConfig.API_HOST;
+
+/**
+ * We use axios request interceptor to set the access token into headers each request the app send
+ */
+axios.interceptors.request.use((config): AxiosRequestConfig => {
+  console.log("coucou petite perruche", config);
+  // get tokens in cookies
+  return config;
+});
