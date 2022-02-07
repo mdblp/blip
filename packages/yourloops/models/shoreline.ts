5e04b47f3f1db106e833a3fa9a7097c1c7e5feb0
Yann Blanc <yann.blanc@diabeloop.fr>
2022-02-07 16:20:58 +0100

    reorder + no item selected + align back next button

--- packages/yourloops/models/shoreline.ts
+++ packages/yourloops/models/shoreline.ts
@@ -48,6 +48,7 @@ enum UserRoles {
   hcp = "hcp",
   caregiver = "caregiver",
   patient = "patient",
+  empty = "",
   /** When the account is created but not yet confirm */
   unverified = "unverified"
 }
