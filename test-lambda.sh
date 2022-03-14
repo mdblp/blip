f4f597d99baeb60119d62c0ee7577bb734bd83d3
Yann Blanc <yann.blanc@diabeloop.fr>
2022-03-14 18:28:48 +0100

    fix lambda tests

--- test-lambda.sh
+++ test-lambda.sh
@@ -8,5 +8,6 @@ export BRANDING='diabeloop_blue'
 rm -rf 'dist'
 mkdir -pv 'dist/static'
 cp -v 'templates/index.html' 'dist/static/'
+cp -v "branding/diabeloop/blue/favicon.ico" 'dist/static/branding_diabeloop_blue_favicon.ico'
 npm run gen-lambda
 mocha test/lambda/cloudfront-gen-lambda.test.js
