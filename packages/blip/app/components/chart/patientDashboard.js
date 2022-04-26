ca7a826dd47d6492b39b145aa73b787ca4c70140
Cedric Perotto <cedric.perotto@diabeloop.fr>
2022-04-26 16:42:11 +0200

    Add basic dashboard page

---
+++ packages/blip/app/components/chart/patientDashboard.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import moment from "moment-timezone";
+import Box from "@material-ui/core/Box";
+import PatientStatistics from "./patientStatistics";
+import Header from "./header";
+
+const PatientDashboard = (props) => {
+  //eslint-disable-next-line
+  const { patient, prefixURL, profileDialog, bgPrefs, loading, chartPrefs, dataUtil, epochLocation, msRange} = props;
+  const getEndpoints = () => {
+    const start = moment.utc(epochLocation - msRange / 2).toISOString();
+    const end = moment.utc(epochLocation + msRange / 2).toISOString();
+    return [start, end];
+  };
+  const endpoints = getEndpoints();
+  return (
+    <div id="patient-dashboard" className="patient-dashboard">
+      <Header
+        profileDialog={profileDialog}
+        chartType={"dashboard"}
+        patient={patient}
+        prefixURL={prefixURL}
+        canPrint={true}
+      />
+      <Box id="patient-dashboard-content">
+        <PatientStatistics
+          bgPrefs={bgPrefs}
+          //eslint-disable-next-line
+          bgSource={dataUtil.bgSource}
+          chartPrefs={chartPrefs}
+          chartType="daily"
+          dataUtil={dataUtil}
+          endpoints={endpoints}
+          loading={loading}
+        />
+        {/*<chatWidget/>*/}
+      </Box>
+    </div>
+  );
+};
+
+PatientDashboard.propType = {
+  loading: PropTypes.bool.isRequired,
+  patient: PropTypes.object,
+  prefixURL:  PropTypes.string,
+  profileDialog: PropTypes.func,
+  bgPrefs: PropTypes.object.isRequired,
+  chartPrefs: PropTypes.object.isRequired,
+  dataUtil: PropTypes.object,
+  epochLocation: PropTypes.number.isRequired,
+  msRange: PropTypes.number.isRequired,
+};
+
+export default PatientDashboard;
---
+++ packages/blip/app/components/chart/patientStatistics.js
@@ -0,0 +1,60 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+import VisibilityIcon from "@material-ui/icons/Visibility";
+import { useTranslation } from "react-i18next";
+import Stats from "./stats";
+
+const useStyles = makeStyles(() => ({
+  card: {
+    maxWidth: 345,
+  },
+  cardContent: {
+    overflowY: "auto",
+    maxHeight: 500,
+  },
+}));
+
+const PatientStatistics = (props) => {
+  //eslint-disable-next-line
+  const { bgPrefs, loading, chartPrefs, dataUtil, endpoints } = props;
+  const { t } = useTranslation("yourloops");
+  const classes = useStyles();
+  return (
+    <Card className={classes.card}>
+      <CardHeader
+        avatar={<VisibilityIcon/>}
+        title={t("Overview")}
+        action={
+          <Typography variant="overline" display="block">{t("Show More")}</Typography>
+        }
+      />
+      <CardContent className={classes.cardContent}>
+        <Stats
+          bgPrefs={bgPrefs}
+          //eslint-disable-next-line
+          bgSource={dataUtil.bgSource}
+          chartPrefs={chartPrefs}
+          chartType="daily"
+          dataUtil={dataUtil}
+          endpoints={endpoints}
+          loading={loading}
+        />
+      </CardContent>
+    </Card>
+  );
+};
+
+PatientStatistics.propType = {
+  bgPrefs: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired,
+  chartPrefs: PropTypes.object.isRequired,
+  dataUtil: PropTypes.object.isRequired,
+  endpoints: PropTypes.arrayOf(PropTypes.string),
+};
+
+export default PatientStatistics;
