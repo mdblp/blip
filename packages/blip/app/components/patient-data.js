dab419f231fe58e9aa33261b426dd3a6cd6c3ee1
Yann BLANC <yann.blanc@diabeloop.fr>
2022-05-05 11:47:26 +0200

    YLP-1370 hcp and patient can communicate through messaging system (#798)
    Add chat widget in dashboard for HCP and patient

--- packages/blip/app/components/patient-data.js
+++ packages/blip/app/components/patient-data.js
@@ -335,7 +335,7 @@ class PatientDataPage extends React.Component {
   }
 
   renderChart() {
-    const { patient, profileDialog, prefixURL, dialogDatePicker, dialogRangeDatePicker } = this.props;
+    const { patient, teams, profileDialog, prefixURL, dialogDatePicker, dialogRangeDatePicker, chatWidget, api } = this.props;
     const {
       canPrint,
       permsOfLoggedInUser,
@@ -346,6 +346,7 @@ class PatientDataPage extends React.Component {
       msRange,
       tidelineData,
     } = this.state;
+    const user = api.whoami;
 
     return (
       <Switch>
@@ -354,6 +355,8 @@ class PatientDataPage extends React.Component {
             bgPrefs={this.state.bgPrefs}
             chartPrefs={chartPrefs}
             patient={patient}
+            teams={teams}
+            user={user}
             dataUtil={this.dataUtil}
             timePrefs={this.state.timePrefs}
             epochLocation={epochLocation}
@@ -363,6 +366,7 @@ class PatientDataPage extends React.Component {
             tidelineData={tidelineData}
             permsOfLoggedInUser={permsOfLoggedInUser}
             trackMetric={this.trackMetric}
+            chatWidget={chatWidget}
           />
         </Route>
         <Route path={`${prefixURL}/overview`}>
@@ -999,6 +1003,8 @@ class PatientDataPage extends React.Component {
 
 PatientDataPage.propTypes = {
   api: PropTypes.object.isRequired,
+  teams: PropTypes.object.isRequired,
+  chatWidget: PropTypes.object.isRequired,
   patient: PropTypes.object.isRequired,
   store: PropTypes.object.isRequired,
   profileDialog: PropTypes.func.isRequired,
---
+++ packages/yourloops/models/chat.ts
@@ -0,0 +1,16 @@
+import User from "../lib/auth/user";
+
+/**
+ * Message interface
+ */
+export interface IMessage {
+  readonly id: string;
+  patientId: string;
+  teamId: string;
+  authorId: string;
+  destAck: boolean;
+  text: string;
+  timezone: string;
+  timestamp?: string;
+  user: User;
+}
