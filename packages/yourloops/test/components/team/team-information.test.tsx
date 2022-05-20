9b77a2c16053ef01c79c09a08ed143ee6968dc3f
Tim Canu <timothee.canu@diabeloop.fr>
2022-05-20 11:00:02 +0200

    Added missing UTs for component team-information

--- packages/yourloops/test/components/team/team-information.test.tsx
+++ packages/yourloops/test/components/team/team-information.test.tsx
@@ -26,55 +26,123 @@
  */
 
 import React from "react";
-import renderer from "react-test-renderer";
-import { teamAPI } from "../../lib/team/utils";
-import { TeamContextProvider } from "../../../lib/team";
-import { AuthContext, AuthContextProvider } from "../../../lib/auth";
-import { loggedInUsers } from "../../common";
-import { createAuthHookStubs } from "../../lib/auth/utils";
-import { getTheme } from "../../../components/theme";
-import ThemeProvider from "@material-ui/styles/ThemeProvider";
-import TeamInformation, { TeamInformationProps } from "../../../components/team/team-information";
-import { buildTeam } from "../../common/utils";
+import ReactDOM from "react-dom";
+import { act } from "react-dom/test-utils";
 
+import ThemeProvider from "@material-ui/styles/ThemeProvider";
+
+import * as teamHookMock from "../../../lib/team";
+import { TeamContextProvider } from "../../../lib/team";
+import * as authHookMock from "../../../lib/auth";
+import { AuthContextProvider } from "../../../lib/auth";
+import { getTheme } from "../../../components/theme";
+import TeamInformation, { TeamInformationProps } from "../../../components/team/team-information";
+import { buildTeam, triggerMouseEvent } from "../../common/utils";
+import User from "../../../lib/auth/user";
+
+jest.mock("../../../lib/team");
+jest.mock("../../../lib/auth");
 describe("TeamInformation", () => {
-  const authHcp = loggedInUsers.hcpSession;
-  const authHookHcp: AuthContext = createAuthHookStubs(authHcp);
   const refresh = jest.fn();
 
-  function renderTeamInformation(props: TeamInformationProps) {
-    return renderer.create(
-      <ThemeProvider theme={getTheme()}>
-        <AuthContextProvider value={authHookHcp}>
-          <TeamContextProvider teamAPI={teamAPI}>
-            <TeamInformation
-              team={props.team}
-              refreshParent={props.refreshParent}
-            />
-          </TeamContextProvider>
-        </AuthContextProvider>
-      </ThemeProvider>
-    );
+  const teamId = "teamId";
+  const team = buildTeam(teamId, []);
+  team.phone = "012345678";
+  team.address = {
+    line1: "line 1",
+    line2: "line 2",
+    zip: "08130",
+    city: "Vouilly",
+    country: "France",
+  };
+
+  let container: HTMLDivElement | null = null;
+
+  beforeEach(() => {
+    (authHookMock.AuthContextProvider as jest.Mock) = jest.fn().mockImplementation(({ children }) => {
+      return children;
+    });
+    (authHookMock.useAuth as jest.Mock).mockImplementation(() => {
+      return { user : { isUserPatient : () => true } as User };
+    });
+    (teamHookMock.TeamContextProvider as jest.Mock) = jest.fn().mockImplementation(({ children }) => {
+      return children;
+    });
+    (teamHookMock.useTeam as jest.Mock).mockImplementation(() => {
+      return { isUserAdministrator: jest.fn().mockReturnValue(true) };
+    });
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    if (container) {
+      ReactDOM.unmountComponentAtNode(container);
+      document.body.removeChild(container);
+      container = null;
+    }
+  });
+
+  function getTeamInformationJSX(props: TeamInformationProps) {
+    return <ThemeProvider theme={getTheme()}>
+      <AuthContextProvider>
+        <TeamContextProvider>
+          <TeamInformation
+            team={props.team}
+            refreshParent={props.refreshParent}
+          />
+        </TeamContextProvider>
+      </AuthContextProvider>
+    </ThemeProvider>;
+  }
+
+  function renderTeamInformation(props: TeamInformationProps = { team, refreshParent: refresh }) {
+    act(() => {
+      ReactDOM.render(getTeamInformationJSX(props), container);
+    });
   }
 
   it("should display correct team information", () => {
-    const teamId = "teamId";
-    const team = buildTeam(teamId, []);
-    team.phone = "012345678";
-    team.address = {
-      line1: "line 1",
-      line2: "line 2",
-      zip: "08130",
-      city: "Vouilly",
-      country: "France",
-    };
-    const props: TeamInformationProps = { team, refreshParent: refresh };
     const address = `${team.address?.line1}\n${team.address?.line2}\n${team.address?.zip}\n${team.address?.city}\n${team.address?.country}`;
-    const component = renderTeamInformation(props);
-    expect(component.root.findByProps({ id: `team-information-${teamId}-name` }).props.children).toEqual(team.name);
-    expect(component.root.findByProps({ id: `team-information-${teamId}-phone` }).props.children).toEqual(team.phone);
-    expect(component.root.findByProps({ id: `team-information-${teamId}-code` }).props.children).toEqual(team.code);
-    expect(component.root.findByProps({ id: `team-information-${teamId}-address` }).props.children).toEqual(address);
+    renderTeamInformation();
+    expect(document.getElementById(`team-information-${teamId}-name`).innerHTML).toEqual(team.name);
+    expect(document.getElementById(`team-information-${teamId}-phone`).innerHTML).toEqual(team.phone);
+    expect(document.getElementById(`team-information-${teamId}-code`).innerHTML).toEqual(team.code);
+    expect(document.getElementById(`team-information-${teamId}-address`).innerHTML).toEqual(address);
+  });
+
+  it("should display button to edit team when user is admin", () => {
+    renderTeamInformation();
+    expect(document.getElementById("edit-team-button")).not.toBeNull();
+  });
+
+  it("should open edit team dialog when clicking on edit team button", () => {
+    renderTeamInformation();
+    act(() => {
+      triggerMouseEvent("click", document.getElementById("edit-team-button"));
+    });
+    expect(document.getElementById("team-edit-dialog")).not.toBeNull();
+  });
+
+  it("should not display button to edit team when user is not admin", () => {
+    (teamHookMock.useTeam as jest.Mock).mockImplementation(() => {
+      return { isUserAdministrator: jest.fn().mockReturnValue(false) };
+    });
+    renderTeamInformation();
+    expect(document.getElementById("edit-team-button")).toBeNull();
+  });
+
+  it("should display button to leave team when user is patient", () => {
+    renderTeamInformation();
+    expect(document.getElementById("leave-team-button")).not.toBeNull();
+  });
+
+  it("should not display button to leave team when user is not patient", () => {
+    (authHookMock.useAuth as jest.Mock).mockImplementation(() => {
+      return { user : { isUserPatient : () => false } as User };
+    });
+    renderTeamInformation();
+    expect(document.getElementById("leave-team-button")).toBeNull();
   });
 });
 
