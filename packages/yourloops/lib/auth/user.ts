c15059f871e254dec229b8f207d0f5d3737cd37a
arnaud <arno.tgh@gmail.com>
2022-06-28 10:07:29 +0200

    PR review

--- packages/yourloops/lib/auth/user.ts
+++ packages/yourloops/lib/auth/user.ts
@@ -1,3 +1,30 @@
+/**
+ * Copyright (c) 2022, Diabeloop
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 import {
   AuthenticatedUser,
   Consent,
@@ -80,7 +107,7 @@ export default class User {
   }
 
   /**
-   * Check If the user should accept is consent at a first login.
+   * Check If the user should accept his consent at a first login.
    * @description the first login is determined by null consents object
    */
   shouldAcceptConsent(): boolean {
@@ -88,7 +115,7 @@ export default class User {
   }
 
   /**
-   * Check If the user should renew is consent.
+   * Check If the user should renew his consent.
    */
   shouldRenewConsent(): boolean {
     if (!this.profile?.termsOfUse || !this.profile.privacyPolicy) {
