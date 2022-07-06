6d3f1ba634416a7a2f53484115caccc0b509e6ce
arnaud <arno.tgh@gmail.com>
2022-07-06 17:07:57 +0200

    new typescript configuration for yourloops package

--- packages/yourloops/webpack.config.js
+++ packages/yourloops/webpack.config.js
@@ -252,7 +252,7 @@ const webpackConfig = {
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-      { test: /\.tsx?$/, loader: "ts-loader", exclude: /node_modules/ },
+      { test: /\.tsx?$/, loader: "ts-loader", exclude: /node_modules/, options: { configFile: "tsconfig.build.json" } },
       blipWebpack.babelLoaderConfiguration,
       blipWebpack.lessLoaderConfiguration,
       blipWebpack.cssLoaderConfiguration,
