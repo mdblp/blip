074cca18aeb2df6048c0c413cfee55c41a220fb0
arnaud <arno.tgh@gmail.com>
2022-08-09 13:38:38 +0200

    adding team api tests

--- packages/yourloops/test/lib/team/team-api.test.tsx
+++ packages/yourloops/test/lib/team/team-api.test.tsx
@@ -27,9 +27,128 @@
 
 import TeamApi from '../../../lib/team/team-api'
 import { Monitoring } from '../../../models/monitoring'
-import HttpService from '../../../services/http'
+import HttpService, { ErrorMessageStatus } from '../../../services/http'
+import { ITeam, ITeamMember, TeamMemberRole, TeamType } from '../../../models/team'
+import { AxiosResponse } from 'axios'
+import { INotificationAPI } from '../../../models/notification'
+import { UserRoles } from '../../../models/user'
+import { HttpHeaderKeys } from '../../../models/api'
+import { getCurrentLang } from '../../../lib/language'
+import { PostalAddress } from '../../../models/generic'
 
 describe('TeamApi', () => {
+  const userId = 'userId'
+  const teamId = 'teamId'
+  const email = 'email@test.com'
+  const role = TeamMemberRole.admin
+  const patientId = 'patientId'
+  const monitoring = { enabled: true } as Monitoring
+
+  describe('getTeams', () => {
+    it('should get a list a teams', async () => {
+      const data: ITeam[] = [
+        { name: 'team1' } as ITeam,
+        { name: 'team2' } as ITeam
+      ]
+      jest.spyOn(HttpService, 'get').mockResolvedValueOnce({ data } as AxiosResponse)
+
+      const teams = await TeamApi.getTeams()
+      expect(teams).toEqual(data)
+      expect(HttpService.get).toHaveBeenCalledWith({ url: '/v0/my-teams' })
+    })
+  })
+
+  describe('getPatients', () => {
+    it('should get a list a patients', async () => {
+      const data: ITeamMember[] = [
+        { email: 'member1@team.com' } as ITeamMember,
+        { email: 'member2@team.com' } as ITeamMember
+      ]
+      jest.spyOn(HttpService, 'get').mockResolvedValueOnce({ data } as AxiosResponse)
+
+      const patients = await TeamApi.getPatients()
+      expect(patients).toEqual(data)
+      expect(HttpService.get).toHaveBeenCalledWith({ url: '/v0/my-patients' })
+    })
+  })
+
+  describe('invitePatient', () => {
+    it('should invite a new patient in a team and get a notification if success', async () => {
+      const data = { creatorId: 'creatorId' } as INotificationAPI
+      jest.spyOn(HttpService, 'post').mockResolvedValueOnce({ data } as AxiosResponse)
+
+      const notification = await TeamApi.invitePatient({ teamId, email })
+      expect(notification).toEqual(data)
+      expect(HttpService.post).toHaveBeenCalledWith({
+        url: '/confirm/send/team/invite',
+        payload: { teamId, email, role: UserRoles.patient },
+        config: { headers: { [HttpHeaderKeys.language]: getCurrentLang() } }
+      })
+    })
+  })
+
+  describe('inviteMember', () => {
+    it('should invite a new member in a team and get a notification if success', async () => {
+      const data = { creatorId: 'creatorId' } as INotificationAPI
+      jest.spyOn(HttpService, 'post').mockResolvedValueOnce({ data } as AxiosResponse)
+
+      const notification = await TeamApi.inviteMember({ teamId, email, role })
+      expect(notification).toEqual(data)
+      expect(HttpService.post).toHaveBeenCalledWith({
+        url: '/confirm/send/team/invite',
+        payload: { teamId, email, role },
+        config: { headers: { [HttpHeaderKeys.language]: getCurrentLang() } }
+      })
+    })
+  })
+
+  describe('createTeam', () => {
+    const name = 'super team'
+    const phone = 'iPhone 5'
+    const address = { city: 'grenoble' } as PostalAddress
+    const newTeam = { name, phone, address } as ITeam
+
+    it('should create a new team', async () => {
+      jest.spyOn(HttpService, 'post').mockResolvedValueOnce({ data: newTeam } as AxiosResponse)
+      const team = await TeamApi.createTeam(newTeam)
+      expect(team).toEqual(newTeam)
+      expect(HttpService.post).toHaveBeenCalledWith({
+        url: '/crew/v0/teams',
+        payload: { ...newTeam, type: TeamType.medical }
+      })
+    })
+
+    it('should failed if missing mandatory parameters', async () => {
+      const wrongTeam = { name }
+      await expect(async () => {
+        await TeamApi.createTeam(wrongTeam)
+      }).rejects.toThrow()
+    })
+  })
+
+  describe('editTeam', () => {
+    it('should edit a team', async () => {
+      const editedTeam = { name: 'updated name', id: '1234' } as ITeam
+      jest.spyOn(HttpService, 'put').mockResolvedValue(undefined)
+      await TeamApi.editTeam(editedTeam)
+      expect(HttpService.put).toHaveBeenCalledWith({
+        url: `/crew/v0/teams/${editedTeam.id}`,
+        payload: editedTeam
+      })
+    })
+  })
+
+  describe('updatePatientAlerts', () => {
+    it('should update patient alerts', async () => {
+      jest.spyOn(HttpService, 'put').mockResolvedValueOnce(undefined)
+      await TeamApi.updatePatientAlerts(teamId, patientId, monitoring)
+      expect(HttpService.put).toHaveBeenCalledWith({
+        url: `/crew/v0/teams/${teamId}/patients/${patientId}/monitoring`,
+        payload: monitoring
+      })
+    })
+  })
+
   describe('updateTeamAlerts', () => {
     it('should make correct http call', () => {
       const teamId = 'fakeTeamId'
@@ -43,6 +162,88 @@ describe('TeamApi', () => {
       TeamApi.updateTeamAlerts(teamId, monitoring)
 
       expect(httpServiceSpy).toHaveBeenCalledWith(expectedParams)
+    })
+  })
+
+  describe('deleteTeam', () => {
+    it('should delete a team', async () => {
+      jest.spyOn(HttpService, 'delete').mockResolvedValueOnce(undefined)
+      await TeamApi.deleteTeam(teamId)
+      expect(HttpService.delete).toHaveBeenCalledWith({ url: `/crew/v0/teams/${teamId}` })
+    })
+  })
+
+  describe('leaveTeam', () => {
+    it('should leave a team', async () => {
+      jest.spyOn(HttpService, 'delete').mockResolvedValueOnce(undefined)
+      await TeamApi.leaveTeam(userId, teamId)
+      expect(HttpService.delete).toHaveBeenCalledWith({ url: `/crew/v0/teams/${teamId}/members/${userId}` })
+    })
+  })
+
+  describe('removeMember', () => {
+    it('should remove a member from a team', async () => {
+      jest.spyOn(HttpService, 'delete').mockResolvedValueOnce(undefined)
+      await TeamApi.removeMember({ teamId, userId, email })
+      expect(HttpService.delete).toHaveBeenCalledWith({
+        url: `confirm/send/team/leave/${teamId}/${userId}`,
+        config: { params: { email } }
+      })
+    })
+  })
+
+  describe('removePatient', () => {
+    it('should remove a patient from a team', async () => {
+      jest.spyOn(HttpService, 'delete').mockResolvedValueOnce(undefined)
+      await TeamApi.removePatient(teamId, userId)
+      expect(HttpService.delete).toHaveBeenCalledWith({ url: `/crew/v0/teams/${teamId}/patients/${userId}` })
+    })
+  })
+
+  describe('changeMemberRole', () => {
+    it('should change the member role in the team', async () => {
+      const httpCall = jest.spyOn(HttpService, 'put').mockResolvedValueOnce(undefined)
+      await TeamApi.changeMemberRole({ teamId, userId, email, role })
+      expect(httpCall).toHaveBeenNthCalledWith(1, {
+        url: `/confirm/send/team/role/${userId}`,
+        payload: { teamId, email, role }
+      })
+      expect(httpCall).toHaveBeenNthCalledWith(2, {
+        url: `/crew/v0/teams/${teamId}/members`,
+        payload: { teamId, userId, role }
+      })
+    })
+  })
+
+  describe('getTeamFromCode', () => {
+    const code = '123 456 789'
+    const team = { code } as ITeam
+
+    it('should get a team with if exists', async () => {
+      jest.spyOn(HttpService, 'get').mockResolvedValueOnce({ data: [team] } as AxiosResponse)
+      const response = await TeamApi.getTeamFromCode(code)
+      expect(response).toEqual(team)
+      expect(HttpService.get).toHaveBeenCalledWith({
+        url: '/crew/v0/teams',
+        config: { params: { code } }
+      })
+    })
+
+    it('should return null if team doesn\'t exists', async () => {
+      jest.spyOn(HttpService, 'get').mockRejectedValueOnce(Error(ErrorMessageStatus.NotFound))
+      const response = await TeamApi.getTeamFromCode(code)
+      expect(response).toEqual(null)
+    })
+  })
+
+  describe('joinTeam', () => {
+    it('should join a team', async () => {
+      jest.spyOn(HttpService, 'put').mockResolvedValueOnce(undefined)
+      await TeamApi.joinTeam(teamId, userId)
+      expect(HttpService.put).toHaveBeenCalledWith({
+        url: `/crew/v0/teams/${teamId}/patients`,
+        payload: { userId }
+      })
     })
   })
 })
