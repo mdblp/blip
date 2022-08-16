e19ad2bf0a2bbeb93040b488062b221cf127adfe
Tim Canu <timothee.canu@diabeloop.fr>
2022-08-16 08:44:47 +0200

    Proposition: get rid of coverage reporter for packages other than yourloops (because not used and broken)

--- karma.common.conf.js
+++ karma.common.conf.js
@@ -26,18 +26,18 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-const path = require("path");
-const _ = require("lodash");
+const path = require('path')
+const _ = require('lodash')
 
-const isWSL = _.isString(process.env.WSL_DISTRO_NAME);
-const browsers = ["CustomChromeHeadless"];
+const isWSL = _.isString(process.env.WSL_DISTRO_NAME)
+const browsers = ['CustomChromeHeadless']
 if (!isWSL) {
-  browsers.push("FirefoxHeadless");
+  browsers.push('FirefoxHeadless')
 }
 
 const defaultConfig = {
   autoWatch: false,
-  port: "8080",
+  port: '8080',
   browserNoActivityTimeout: 60000,
   captureTimeout: 60000,
   colors: true,
@@ -46,92 +46,46 @@ const defaultConfig = {
   browsers,
   customLaunchers: {
     CustomChromeHeadless: {
-      base: "ChromeHeadless",
+      base: 'ChromeHeadless',
       flags: [
-        "--enable-automation",
-        "--no-default-browser-check",
-        "--no-first-run",
-        "--disable-default-apps",
-        "--disable-popup-blocking",
-        "--disable-translate",
-        "--disable-background-timer-throttling",
-        "--disable-renderer-backgrounding",
-        "--disable-device-discovery-notifications",
-        "--disable-dev-shm-usage",
-        "--disable-gpu",
-        "--headless",
-        "--no-sandbox",
-        "--remote-debugging-port=9222",
-      ],
-    },
-  },
-  coverageIstanbulReporter: {
-    // reports can be any that are listed here: https://github.com/istanbuljs/istanbuljs/tree/73c25ce79f91010d1ff073aa6ff3fd01114f90db/packages/istanbul-reports/lib
-    "reports": ["html", "text-summary"],
-    // base output directory. If you include %browser% in the path it will be replaced with the karma browser name
-    "dir": "coverage",
-    // Combines coverage information from multiple browsers into one report rather than outputting a report for each browser.
-    "combineBrowserReports": true,
-    // if using webpack and pre-loaders, work around webpack breaking the source path
-    "fixWebpackSourcePaths": false,
-    // Omit files with no statements, no functions and no branches covered from the report
-    "skipFilesWithNoCoverage": true,
-    // Most reporters accept additional config options. You can pass these through the `report-config` option
-    "report-config": {
-      // all options available at: https://github.com/istanbuljs/istanbuljs/blob/73c25ce79f91010d1ff073aa6ff3fd01114f90db/packages/istanbul-reports/lib/html/index.js#L257-L261
-      html: {
-        // outputs the report in ./coverage/html
-        // subdir: "html",
-      },
-    },
-    // enforce percentage thresholds
-    // anything under these percentages will cause karma to fail with an exit code of 1 if not running in watch mode
-    "thresholds": {
-      emitWarning: true, // set to `true` to not fail the test command when thresholds are not met
-      // thresholds for all files
-      global: {
-        statements: 90,
-        lines: 95,
-        branches: 95,
-        functions: 100,
-      },
-      // thresholds per file
-      // each: {
-      //   statements: 100,
-      //   lines: 100,
-      //   branches: 100,
-      //   functions: 100,
-      //   overrides: {
-      //     'baz/component/**/*.js': {
-      //       statements: 98
-      //     }
-      //   }
-      // },
-    },
-
-    "verbose": true, // output config used by istanbul for debugging
+        '--enable-automation',
+        '--no-default-browser-check',
+        '--no-first-run',
+        '--disable-default-apps',
+        '--disable-popup-blocking',
+        '--disable-translate',
+        '--disable-background-timer-throttling',
+        '--disable-renderer-backgrounding',
+        '--disable-device-discovery-notifications',
+        '--disable-dev-shm-usage',
+        '--disable-gpu',
+        '--headless',
+        '--no-sandbox',
+        '--remote-debugging-port=9222'
+      ]
+    }
   },
   junitReporter: {
     logReport: false,
-    name: "yourloops",
-    filename: "junit.xml",
+    name: 'yourloops',
+    filename: 'junit.xml'
   },
   mime: {
-    "text/x-typescript": ["ts", "tsx"],
+    'text/x-typescript': ['ts', 'tsx']
   },
-  frameworks: ["mocha"],
-  reporters: ["mocha", "coverage-istanbul", "junit"],
+  frameworks: ['mocha'],
+  reporters: ['mocha', 'junit'],
   webpackMiddleware: {
     noInfo: true,
-    stats: "errors-only",
+    stats: 'errors-only'
   },
   plugins: [
-    "karma-*",
-    require("./karma.junit.reporter"),
-  ],
-};
+    'karma-*',
+    require('./karma.junit.reporter')
+  ]
+}
 
-let updatedConfig = null;
+let updatedConfig = null
 
 /**
  * @param {string} projectName "yourloops" | "blip" | "viz" | "tideline"
@@ -140,40 +94,29 @@ let updatedConfig = null;
  * @param {boolean} typescriptProject true for typescript project
  */
 function updateDefaultConfig(projectName, karmaConfig, webpackConfig, typescriptProject = false) {
-  delete webpackConfig.entry;
-  delete webpackConfig.output;
-  webpackConfig.devtool = "inline-source-map";
+  delete webpackConfig.entry
+  delete webpackConfig.output
+  webpackConfig.devtool = 'inline-source-map'
   if (typescriptProject) {
-    webpackConfig.module.rules[0].options = { configFile: "test/tsconfig.test.json" };
+    webpackConfig.module.rules[0].options = { configFile: 'test/tsconfig.test.json' }
   }
-  updatedConfig = _.defaultsDeep(karmaConfig, defaultConfig);
+  updatedConfig = _.defaultsDeep(karmaConfig, defaultConfig)
 
   // special config for unit test debug
-  if (process.env.DEBUG_UNIT_TEST === "true") {
+  if (process.env.DEBUG_UNIT_TEST === 'true') {
     // since we're in debug, we do not want to run tests in headless
     // we wait for a manual chrome visit to localhost:9876 to start tests
-    updatedConfig.browsers = [];
-    updatedConfig.customLaunchers = {};
-    updatedConfig.port = "9876";
-    updatedConfig.reporters = ["mocha", "junit", "progress"];
-  } else {
-    updatedConfig.coverageIstanbulReporter.dir = path.join(__dirname, `coverage/${projectName}`);
-    webpackConfig.module.rules.push({
-      test: typescriptProject ? /\.tsx?$/ : /\.jsx?$/,
-      exclude: /node_modules/,
-      loader: "@jsdevtools/coverage-istanbul-loader",
-      enforce: "post",
-      options: {
-        esModules: true,
-      },
-    });
+    updatedConfig.browsers = []
+    updatedConfig.customLaunchers = {}
+    updatedConfig.port = '9876'
+    updatedConfig.reporters = ['mocha', 'junit', 'progress']
   }
 
-  updatedConfig.files.unshift("../../node-compat.js");
-  updatedConfig.files.unshift("../../branding/**/*.css");
-  updatedConfig.webpack = webpackConfig;
-  updatedConfig.junitReporter.name = projectName;
-  updatedConfig.junitReporter.filename = path.join(__dirname, `reports/${projectName}.junit.xml`);
+  updatedConfig.files.unshift('../../node-compat.js')
+  updatedConfig.files.unshift('../../branding/**/*.css')
+  updatedConfig.webpack = webpackConfig
+  updatedConfig.junitReporter.name = projectName
+  updatedConfig.junitReporter.filename = path.join(__dirname, `reports/${projectName}.junit.xml`)
 }
 
 /**
@@ -181,8 +124,8 @@ function updateDefaultConfig(projectName, karmaConfig, webpackConfig, typescript
  * @param {object} config
  */
 function setKarmaConfig(config) {
-  updatedConfig.logLevel = config.LOG_INFO;
-  config.set(updatedConfig);
+  updatedConfig.logLevel = config.LOG_INFO
+  config.set(updatedConfig)
 }
 
-module.exports = { updateDefaultConfig, setKarmaConfig };
+module.exports = { updateDefaultConfig, setKarmaConfig }
