5a0d2ed44371f63ce87cebc8ad490642f4eeeeaf
Tim Canu <timothee.canu@diabeloop.fr>
2022-08-26 10:21:39 +0200

    Fixed jest coverage configuration

--- packages/yourloops/test/integration/jest.config.js
+++ packages/yourloops/test/integration/jest.config.js
@@ -1,11 +1,7 @@
 const commonJestConfig = require('../common-jest.config')
 module.exports = {
   ...commonJestConfig,
-  // ...
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: [
-    '<rootDir>/../**/**.spec.ts*'
-  ],
+
   displayName: 'yourloops integration',
 
   // The glob patterns Jest uses to detect test files
