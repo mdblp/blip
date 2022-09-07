7cebf1cce355494935ff240db415cd009aff2079
Cedric Perotto <cedric.perotto@diabeloop.fr>
2022-09-07 09:15:35 +0200

    Fix Parameters (DeviceParameters group + pumpSettings)

--- packages/medical-domain/src/domains/models/medical/datum/DeviceParameterChange.ts
+++ packages/medical-domain/src/domains/models/medical/datum/DeviceParameterChange.ts
@@ -3,13 +3,18 @@ import BaseDatum from './basics/BaseDatum'
 type DeviceParameterChange = BaseDatum & {
   type: 'deviceEvent'
   subType: 'deviceParameter'
-  uploadId: string
-  name: string
-  level: string
-  units: string
-  value: string
-  previousValue: string
-  lastUpdateDate: string
+  params: Array<{
+    id: string
+    epoch: number
+    timezone: string
+    uploadId: string
+    name: string
+    level: string
+    units: string
+    value: string
+    previousValue: string
+    lastUpdateDate: string
+  }>
 }
 
 export default DeviceParameterChange
