908b9007e6972e6e66295c389816a02709211ac6
arnaud <arno.tgh@gmail.com>
2022-09-12 17:54:25 +0200

    lint fix

--- packages/yourloops/pages/home-page.tsx
+++ packages/yourloops/pages/home-page.tsx
@@ -34,7 +34,6 @@ import { useTranslation } from 'react-i18next'
 import Alert from '@material-ui/lab/Alert'
 import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
 
 import { PatientFilterTypes } from '../models/generic'
 import metrics from '../lib/metrics'
