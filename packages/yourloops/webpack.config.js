60c234e4e014f86a266170bd4af91aee458d4d65
Cedric Perotto <cedric.perotto@diabeloop.fr>
2022-09-13 16:58:58 +0200

    Review webpack/tsconfig for HMR

--- packages/yourloops/webpack.config.js
+++ packages/yourloops/webpack.config.js
@@ -258,7 +258,15 @@ const webpackConfig = {
   module: {
     rules: [
       // All files with a '.ts' or '.tsx' extension will be handled by 'ts-loader'.
-      { test: /\.tsx?$/, loader: 'ts-loader', exclude: /node_modules/, options: { configFile: 'tsconfig.json' } },
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          projectReferences: true,
+          configFile: 'tsconfig.json'
+        }
+      },
       blipWebpack.babelLoaderConfiguration,
       blipWebpack.lessLoaderConfiguration,
       blipWebpack.cssLoaderConfiguration,
