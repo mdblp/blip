9dcd314ad542609fd38b80c162dee2730034e796
Tim Canu <timothee.canu@diabeloop.fr>
2022-09-14 11:54:26 +0200

    Improved beforeEach.js of integration tests

--- packages/yourloops/test/integration/beforeEach.js
+++ packages/yourloops/test/integration/beforeEach.js
@@ -3,9 +3,10 @@ import yourloopsEn from '../../../../locales/en/yourloops.json'
 import translationEn from '../../../../locales/en/translation.json'
 import parameterEn from '../../../../locales/en/parameter.json'
 
-const allTranslations = { ...yourloopsEn, ...translationEn, ...parameterEn }
 const options = i18nOptions
-options.resources.en.yourloops = allTranslations
+options.resources.en.yourloops = yourloopsEn
+options.resources.en.main = translationEn
+options.resources.en.params = parameterEn
 
 i18nInit(options).then(() => {
   // nothing to do
