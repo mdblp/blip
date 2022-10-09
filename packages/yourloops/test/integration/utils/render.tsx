29c718282fce3e90b093f9b26691df7af41b6037
Tim Canu <timothee.canu@diabeloop.fr>
2022-10-09 15:15:35 +0200

    'render' is already wrapped into 'act'

--- packages/yourloops/test/integration/utils/render.tsx
+++ packages/yourloops/test/integration/utils/render.tsx
@@ -2,7 +2,7 @@ import { Router } from 'react-router-dom'
 import { AuthContextProvider } from '../../../lib/auth'
 import { MainLobby } from '../../../app/main-lobby'
 import { createMemoryHistory } from 'history'
-import { act, render } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import React from 'react'
 
 function getMainLobby(history) {
@@ -17,9 +17,6 @@ function getMainLobby(history) {
 
 export const renderPage = (url: string) => {
   const history = createMemoryHistory({ initialEntries: [url] })
-
-  act(() => {
-    render(getMainLobby(history))
-  })
+  render(getMainLobby(history))
   expect(history.location.pathname).toBe(url)
 }
