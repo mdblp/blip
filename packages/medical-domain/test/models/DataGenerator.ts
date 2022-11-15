aae59dcd64966983a5b86c85d60c57ace308c522
Yann Blanc <yann.blanc@diabeloop.fr>
2022-11-15 11:44:43 +0100

    faker added to package json

--- packages/medical-domain/test/models/DataGenerator.ts
+++ packages/medical-domain/test/models/DataGenerator.ts
@@ -35,7 +35,6 @@ function createBaseData(): BaseDatum {
 function createBaseDurationData(): BaseDatum & Duration {
   const baseData = createBaseData()
   const duration = faker.datatype.number({ min: 60000, max: 300000 })
-  // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
   const epochEnd = baseData.epoch + duration
   const normalEnd = new Date(epochEnd).toISOString()
   return {
@@ -52,7 +51,6 @@ function createBaseDurationData(): BaseDatum & Duration {
 function createRandomBasal(): Basal {
   const baseData = createBaseData()
   const duration = faker.datatype.number({ min: 60000, max: 300000 })
-  // eslint-disable-next-line @typescript-eslint/restrict-plus-operands
   const epochEnd = baseData.epoch + duration
   const normalEnd = new Date(epochEnd).toISOString()
   return {
