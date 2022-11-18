9c4286f3c91697d5effa45273a333e96414bcb23
Tim Canu <timothee.canu@diabeloop.fr>
2022-11-18 14:45:56 +0100

    Fixed failing test

--- packages/yourloops/test/unit/lib/notifications/utils.test.tsx
+++ packages/yourloops/test/unit/lib/notifications/utils.test.tsx
@@ -78,7 +78,7 @@ describe('Notification utils', () => {
         error = reason as Error
       }
       expect(error).not.toBeNull()
-      expect(error.message).toBe(`Invalid notification type ${apiNotification.type}`)
+      expect(error.message).toBe('Invalid notification type')
     })
 
     it("should transform 'careteam_invitation' to 'directInvitation'", () => {
