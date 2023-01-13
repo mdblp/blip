555cdcba3f7a6b06ed8438dc1e66fac3d0dba1c1
xwdrblue <hugo.rodrigues@diabeloop.fr>
2023-01-13 11:05:13 +0100

    correction test

--- packages/yourloops/test/integration/pages/patient-dashboard/patient-dashboard-for-HCP.spec.tsx
+++ packages/yourloops/test/integration/pages/patient-dashboard/patient-dashboard-for-HCP.spec.tsx
@@ -147,7 +147,7 @@ describe('Patient dashboard for HCP', () => {
     expect(dashboard.getByText('Messages')).toBeVisible()
 
     /* Remote Monitoring widget */
-    expect(dashboard.getByTestId('remote-monitoring-card')).toHaveTextContent(`Remote monitoring programRemote monitoring:YesRequesting team:MySecondTeamEnd date:${expectedMonitoringEndDate}Remaining time:a few secondsRenewRemove`)
+    expect(dashboard.getByTestId('remote-monitoring-card')).toHaveTextContent(`Remote monitoring programRemote monitoring:YesRequesting team:MySecondTeamEnd date:${expectedMonitoringEndDate}Remaining time:2 minutesRenewRemove`)
     checkHCPLayout(`${firstName} ${lastName}`)
   })
 
