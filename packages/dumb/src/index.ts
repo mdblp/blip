9a1df629bfa82aa3348e2d1c534478fd96a6c1f1
celia-rouis <116577202+celia-rouis@users.noreply.github.com>
2023-01-23 14:46:43 +0100

    YLP-2050 Migrate trends background to dumb (#1042)

--- packages/dumb/src/index.ts
+++ packages/dumb/src/index.ts
@@ -25,6 +25,7 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+import { Background } from './components/trends/common/background/background'
 import { BloodGlucoseTooltip } from './components/tooltips/blood-glucose-tooltip/blood-glucose-tooltip'
 import { BolusTooltip } from './components/tooltips/bolus-tooltip/bolus-tooltip'
 import { CBGMeanStatMemoized as CBGMeanStat } from './components/stats/cbg-mean/cbg-mean-stat'
@@ -59,6 +60,7 @@ import CbgSliceSegment from './components/trends/cbg/cbg-slice-segment'
 
 export {
   AverageDailyDoseStat,
+  Background,
   BloodGlucoseTooltip,
   BolusTooltip,
   CBGMeanStat,
