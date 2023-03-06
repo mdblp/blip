57c0fc33c19ba7be5cb83be9d9aaded87c7aff28
xwdrblue <hugo.rodrigues@diabeloop.fr>
2023-03-06 16:40:14 +0100

    remove drop down

--- packages/yourloops/pages/team/team-details-page.tsx
+++ packages/yourloops/pages/team/team-details-page.tsx
@@ -28,7 +28,6 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useHistory, useParams } from 'react-router-dom'
-
 import { type Theme } from '@mui/material/styles'
 import { makeStyles } from 'tss-react/mui'
 import ArrowBackIcon from '@mui/icons-material/ArrowBack'
@@ -38,9 +37,7 @@ import GroupOutlinedIcon from '@mui/icons-material/GroupOutlined'
 import IconButton from '@mui/material/IconButton'
 import Typography from '@mui/material/Typography'
 import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
-
 import { type Team, useTeam } from '../../lib/team'
-import BasicDropdown from '../../components/dropdown/basic-dropdown'
 import TeamInformation from '../../components/team/team-information'
 import TeamMembers from '../../components/team/team-members'
 import { commonComponentStyles } from '../../components/common'
@@ -132,11 +129,6 @@ function TeamDetailsPage(): JSX.Element {
     history.push('/')
   }
 
-  const redirectToTeam = (selectedTeam: string): void => {
-    const teamToRedirectTo = getMedicalTeams().find((team: Team) => team.name === selectedTeam)
-    history.push(`/teams/${teamToRedirectTo?.id}`)
-  }
-
   const isMonitoringEnabled = (): boolean => {
     return dropdownData.selectedTeam?.monitoring?.enabled
   }
@@ -153,15 +145,6 @@ function TeamDetailsPage(): JSX.Element {
               size="large">
               <ArrowBackIcon />
             </IconButton>
-            <GroupOutlinedIcon />
-            <Box marginLeft={0.5} marginRight={2}>{t('team')}</Box>
-            <BasicDropdown
-              key={dropdownData.selectedTeam.name}
-              id="team-basic-dropdown"
-              defaultValue={dropdownData.selectedTeam.name}
-              values={dropdownData.teamNames}
-              onSelect={redirectToTeam}
-            />
           </Box>
           <Box display="flex">
             {isUserHcp &&
