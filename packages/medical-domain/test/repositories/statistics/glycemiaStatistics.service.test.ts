b0e0326acfb19c37bd47333731d9fef93ad7e5f1
xwdrblue <hugo.rodrigues@diabeloop.fr>
2023-03-16 11:33:49 +0100

    correction medical-domain tests

--- packages/medical-domain/test/repositories/statistics/glycemiaStatistics.service.test.ts
+++ packages/medical-domain/test/repositories/statistics/glycemiaStatistics.service.test.ts
@@ -258,14 +258,14 @@ describe('GlycemiaStatisticsService getSensorUsage', () => {
     const stats = GlycemiaStatisticsService.getSensorUsage(cbgData, 1, dateFilterOneDay)
     expect(stats).toEqual({
       sensorUsage: MS_IN_MIN * 15 * 3 + MS_IN_MIN * 5 * 2,
-      total: MS_IN_DAY
+      totalUsage: MS_IN_DAY
     })
   })
   it('should return sensor usage when viewing more than 1 day', () => {
     const stats = GlycemiaStatisticsService.getSensorUsage(cbgData, 2, dateFilterTwoDays)
     expect(stats).toEqual({
       sensorUsage: MS_IN_MIN * 15 * 3 + MS_IN_MIN * 5 * 3,
-      total: 2 * MS_IN_DAY
+      totalUsage: 2 * MS_IN_DAY
     })
   })
 })
