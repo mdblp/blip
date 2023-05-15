ed282788b7d2bdca0269ad86d287073c59db8b7d
xwdrblue <hugo.rodrigues@diabeloop.fr>
2023-05-15 16:42:11 +0200

    add a display condition for filters

--- packages/yourloops/components/patient-list/patient-list-header.tsx
+++ packages/yourloops/components/patient-list/patient-list-header.tsx
@@ -267,6 +267,7 @@ export const PatientListHeader: FunctionComponent<PatientListHeaderProps> = (pro
         <PatientFiltersPopover
           anchorEl={filtersRef.current}
           onClose={closeFiltersDialog}
+          isPrivatePracticeTeam={isSelectedTeamPrivate}
         />
       }
       {isColumnSelectorOpened &&
