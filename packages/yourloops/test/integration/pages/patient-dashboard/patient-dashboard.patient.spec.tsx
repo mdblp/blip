e39260f9593c725793dd75448a5e20742493734a
Célia <celia.rouis@diabeloop.fr>
2023-06-09 11:17:29 +0200

    YLP-2253 Dedicated error message for a patient joining a team they're already invited in

--- packages/yourloops/test/integration/pages/patient-dashboard/patient-dashboard.patient.spec.tsx
+++ packages/yourloops/test/integration/pages/patient-dashboard/patient-dashboard.patient.spec.tsx
@@ -146,4 +146,10 @@ describe('Patient dashboard for patient', () => {
 
     await testEmptyMedicalFilesWidgetForPatient()
   })
+
+  // it('should allow the patient to join a care team by code', async () => {
+  //   await act(async () => {
+  //     renderPage(patientDashboardRoute)
+  //   })
+  // })
 })
