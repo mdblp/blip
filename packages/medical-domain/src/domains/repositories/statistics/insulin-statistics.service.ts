e1e12821ec1853aa0bff3610d27ffb474bc95696
Tim Canu <timothee.canu@diabeloop.fr>
2023-07-21 15:59:51 +0200

    Variable renaming

--- packages/medical-domain/src/domains/repositories/statistics/insulin-statistics.service.ts
+++ packages/medical-domain/src/domains/repositories/statistics/insulin-statistics.service.ts
@@ -40,9 +40,9 @@ import { type ParameterConfig } from '../../models/medical/datum/pump-settings.m
 
 function resamplingDuration(basals: Basal[], start: number, end: number): Basal[] {
   return basals.map(basal => {
-    const basalEpoch = basal.epoch < start ? start : basal.epoch
+    const basalEpochStart = basal.epoch < start ? start : basal.epoch
     const basalEpochEnd = basal.epochEnd > end ? end : basal.epochEnd
-    basal.duration = basalEpochEnd - basalEpoch
+    basal.duration = basalEpochEnd - basalEpochStart
     return basal
   })
 }
