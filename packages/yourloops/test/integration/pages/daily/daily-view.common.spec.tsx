3b37e27869d1092ba12013c4b7f7ee09bc53ac0b
Tim <timothee.canu@diabeloop.fr>
2023-08-17 10:41:28 +0200

    Fixed failing test (#1406)

--- packages/yourloops/test/integration/pages/daily/daily-view.common.spec.tsx
+++ packages/yourloops/test/integration/pages/daily/daily-view.common.spec.tsx
@@ -144,7 +144,7 @@ describe('Daily view for anyone', () => {
       // This checks that we tried to generate a pdf
       expect(downloadLinkElement.download).toEqual(`yourloops-report-${patient2AsTeamMember.userId}.pdf`)
       expect(downloadLinkElement.href.length).toBeGreaterThan(18000)
-      expect(downloadLinkElement.href.length).toBeLessThan(18100)
+      expect(downloadLinkElement.href.length).toBeLessThan(18150)
       expect(downloadLinkElement.click).toHaveBeenCalledTimes(1)
 
       await userEvent.click(screen.getByText('Download report'))
