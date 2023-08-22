dae2cf4fbf38230af97f1693965ad445321e35d6
xwdrblue <hugo.rodrigues@diabeloop.fr>
2023-08-22 11:29:11 +0200

    add change PR 1362

--- packages/medical-domain/src/domains/repositories/statistics/insulin-statistics.service.ts
+++ packages/medical-domain/src/domains/repositories/statistics/insulin-statistics.service.ts
@@ -42,13 +42,9 @@ import { TimeService } from '../../../index'
 
 function resamplingDuration(basals: Basal[], start: number, end: number): Basal[] {
   return basals.map(basal => {
-    if (basal.epoch < start) {
-      basal.epoch = start
-    }
-    if (basal.epochEnd > end) {
-      basal.epochEnd = end
-    }
-    basal.duration = basal.epochEnd - basal.epoch
+    const basalEpochStart = basal.epoch < start ? start : basal.epoch
+    const basalEpochEnd = basal.epochEnd > end ? end : basal.epochEnd
+    basal.duration = basalEpochEnd - basalEpochStart
     return basal
   })
 }
