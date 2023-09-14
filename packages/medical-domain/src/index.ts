c406d1744f56f0f122691f54410585578c85fb40
Arnaud <52196301+fatb38@users.noreply.github.com>
2023-09-14 14:28:40 +0200

    Dependencies cleanup and update (#1458)
    * unused dependencies cleanup
    
    * fix test
    
    * removing postcss
    
    * bump version
    
    * fix import

--- packages/medical-domain/src/index.ts
+++ packages/medical-domain/src/index.ts
@@ -83,7 +83,7 @@ import {
 import type BasicData from './domains/repositories/medical/basics-data.service'
 import { CarbsStatisticsService } from './domains/repositories/statistics/carbs-statistics.service'
 import { BasalBolusStatisticsService } from './domains/repositories/statistics/insulin-statistics.service'
-import { applyOffset } from './domains/repositories/time/time.service'
+import { applyOffset, MS_IN_DAY } from './domains/repositories/time/time.service'
 
 export {
   applyOffset,
@@ -141,6 +141,7 @@ export {
   type ParametersChange,
   type PumpSettingsParameter,
   ChangeType,
-  BasalBolusStatisticsService
+  BasalBolusStatisticsService,
+  MS_IN_DAY
 }
 export default MedicalDataService
