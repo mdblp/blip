7e4392fb52e4c4dad2df521921906e2ff841057a
Tim Canu <timothee.canu@diabeloop.fr>
2023-10-06 10:22:11 +0200

    Marked props as readonly

--- packages/yourloops/components/chat/chat-widget.tsx
+++ packages/yourloops/components/chat/chat-widget.tsx
@@ -128,7 +128,7 @@ export interface ChatWidgetProps {
   patient: Patient
 }
 
-function ChatWidget(props: ChatWidgetProps): JSX.Element {
+function ChatWidget(props: Readonly<ChatWidgetProps>): JSX.Element {
   const { t } = useTranslation()
   const { patient } = props
   const { classes } = chatWidgetStyles()
