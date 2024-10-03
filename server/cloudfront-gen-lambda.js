f0929e61a5a74571ef6549cbdb435d1712249719
Célia <celia.rouis@diabeloop.fr>
2024-10-03 16:05:53 +0200

    Little update

--- server/cloudfront-gen-lambda.js
+++ server/cloudfront-gen-lambda.js
@@ -247,7 +247,7 @@ function withFilesList(err, files) {
     process.exitCode = 1
     return
   }
-  const selectedFiles = ["'config.js', 'version', '.well-known/assetlinks.json'"]
+  const selectedFiles = ["'config.js', 'version'"]
   for (const file of files) {
     const filename = path.basename(file)
     if (filename === 'index.html') {
