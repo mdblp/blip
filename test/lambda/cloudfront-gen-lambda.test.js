684df133cc10cc7c79cf1edeb138ddaa9a58e665
Yann Blanc <yann.blanc@diabeloop.fr>
2025-01-16 16:52:25 +0100

    fix test

--- test/lambda/cloudfront-gen-lambda.test.js
+++ test/lambda/cloudfront-gen-lambda.test.js
@@ -1,43 +1,43 @@
 
-const crypto = require("crypto");
-const util = require("util");
-const zlib = require("zlib");
-const chai = require("chai");
-const lambda = require("../../dist/lambda/cloudfront-test-blip-request-viewer");
+const crypto = require('crypto')
+const util = require('util')
+const zlib = require('zlib')
+const chai = require('chai')
+const lambda = require('../../dist/lambda/cloudfront-test-blip-request-viewer')
 
-describe("CloudFront Lambda Generator", function () {
-  const { expect } = chai;
+describe('CloudFront Lambda Generator', function () {
+  const { expect } = chai
   /** @type {(string, object) => Promise<object>} */
-  const handler = util.promisify(lambda.handler);
+  const handler = util.promisify(lambda.handler)
 
   const testBase = {
     Records: [
       {
         cf: {
           config: {
-            distributionId: "TESTS"
+            distributionId: 'TESTS'
           },
           request: {
-            uri: "/",
-            method: "GET",
-            clientIp: "2001:cdba::3257:9652",
+            uri: '/',
+            method: 'GET',
+            clientIp: '2001:cdba::3257:9652',
             headers: {
-              "host": [
+              'host': [
                 {
-                  key: "Host",
-                  value: "d123.cf.net"
+                  key: 'Host',
+                  value: 'd123.cf.net'
                 }
               ],
-              "user-agent": [
+              'user-agent': [
                 {
-                  key: "User-Agent",
-                  value: "Test Agent"
+                  key: 'User-Agent',
+                  value: 'Test Agent'
                 }
               ],
-              "user-name": [
+              'user-name': [
                 {
-                  key: "User-Name",
-                  value: "aws-cloudfront"
+                  key: 'User-Name',
+                  value: 'aws-cloudfront'
                 }
               ]
             }
@@ -45,77 +45,81 @@ describe("CloudFront Lambda Generator", function () {
         }
       }
     ]
-  };
+  }
 
-  let indexHTML = "";
-  it("Should return the index.html in a gzip/base64 content", async () => {
-    const gunzip = util.promisify(zlib.gunzip);
-    const response = await handler(testBase, null);
-    expect(response).to.be.an("object");
-    expect(response.status).to.be.equal(200);
-    expect(response.statusDescription).to.be.equal("OK");
-    expect(response.bodyEncoding).to.be.equal("base64");
-    expect(response.body).to.be.a("string");
-    let b = Buffer.from(response.body, "base64");
-    b = await gunzip(b);
-    indexHTML = b.toString("utf8");
-    expect(indexHTML).to.be.a("string");
-    expect(indexHTML.startsWith("<!DOCTYPE html>")).to.be.true;
-  });
+  let indexHTML = ''
+  it('Should return the index.html in a gzip/base64 content', async () => {
+    const gunzip = util.promisify(zlib.gunzip)
+    const response = await handler(testBase, null)
+    expect(response).to.be.an('object')
+    expect(response.status).to.be.equal(200)
+    expect(response.statusDescription).to.be.equal('OK')
+    expect(response.bodyEncoding).to.be.equal('base64')
+    expect(response.body).to.be.a('string')
+    let b = Buffer.from(response.body, 'base64')
+    b = await gunzip(b)
+    indexHTML = b.toString('utf8')
+    expect(indexHTML).to.be.a('string')
+    expect(indexHTML.startsWith('<!DOCTYPE html>')).to.be.true
+  })
 
-  it("Should return the index.html in a gzip/base64 content for others requested URL", async () => {
-    testBase.Records[0].cf.request.uri = "/patients/abcd/data";
-    const gunzip = util.promisify(zlib.gunzip);
-    const response = await handler(testBase, null);
-    expect(response).to.be.an("object");
-    expect(response.status).to.be.equal(200);
-    expect(response.statusDescription).to.be.equal("OK");
-    expect(response.bodyEncoding).to.be.equal("base64");
-    expect(response.body).to.be.a("string");
-    let b = Buffer.from(response.body, "base64");
-    b = await gunzip(b);
-    let html = b.toString("utf8");
-    expect(html).to.be.a("string");
-    expect(indexHTML.startsWith("<!DOCTYPE html>")).to.be.true;
-    expect(html).to.be.not.equal(indexHTML); // nonce are differents
-  });
+  it('Should return the index.html in a gzip/base64 content for others requested URL', async () => {
+    testBase.Records[0].cf.request.uri = '/patients/abcd/data'
+    const gunzip = util.promisify(zlib.gunzip)
+    const response = await handler(testBase, null)
+    expect(response).to.be.an('object')
+    expect(response.status).to.be.equal(200)
+    expect(response.statusDescription).to.be.equal('OK')
+    expect(response.bodyEncoding).to.be.equal('base64')
+    expect(response.body).to.be.a('string')
+    let b = Buffer.from(response.body, 'base64')
+    b = await gunzip(b)
+    let html = b.toString('utf8')
+    expect(html).to.be.a('string')
+    expect(indexHTML.startsWith('<!DOCTYPE html>')).to.be.true
+    expect(html).to.be.not.equal(indexHTML) // nonce are differents
+  })
 
-  it("Should return the config.js", async () => {
-    testBase.Records[0].cf.request.uri = "/config.js";
-    const response = await handler(testBase, null);
-    expect(response).to.be.an("object");
-    expect(response.status).to.be.equal(200);
-    expect(response.statusDescription).to.be.equal("OK");
-    expect(response.body).to.be.a("string");
-    expect(response.body.startsWith("window.config = {")).to.be.true;
+  it('Should return the config.md5.js', async () => {
+    const configFileName = indexHTML.match(/^.*(config\.[\da-f]{20}\.js).*$/i)
+    if (!configFileName) {
+      expect.fail('config file name not found')
+    }
+    testBase.Records[0].cf.request.uri = `/${configFileName}`
+    const response = await handler(testBase, null)
+    expect(response).to.be.an('object')
+    expect(response.status).to.be.equal(200)
+    expect(response.statusDescription).to.be.equal('OK')
+    expect(response.body).to.be.a('string')
+    expect(response.body.startsWith('window.config = {')).to.be.true
 
-    const hash = crypto.createHash("sha512");
-    hash.update(response.body);
-    const configHash = `sha512-${hash.digest("base64")}`;
+    const hash = crypto.createHash('sha512')
+    hash.update(response.body)
+    const configHash = `sha512-${hash.digest('base64')}`
 
-    expect(indexHTML.indexOf(configHash)).to.be.above(0);
-  });
+    expect(indexHTML.indexOf(configHash)).to.be.above(0)
+  })
 
-  it("Should proceed the request to CloudFront for others requests", async () => {
-    testBase.Records[0].cf.request.uri = "/branding_diabeloop_blue_favicon.ico";
-    const response = await handler(testBase, null);
-    expect(response).to.be.equal(testBase.Records[0].cf.request);
-  });
+  it('Should proceed the request to CloudFront for others requests', async () => {
+    testBase.Records[0].cf.request.uri = '/branding_diabeloop_blue_favicon.ico'
+    const response = await handler(testBase, null)
+    expect(response).to.be.equal(testBase.Records[0].cf.request)
+  })
 
-  it("Should request a redirect for distributions files", async () => {
-    testBase.Records[0].cf.request.uri = "/patients/branding_diabeloop_blue_favicon.ico";
-    const response = await handler(testBase, null);
-    expect(response).to.be.an("object");
+  it('Should request a redirect for distributions files', async () => {
+    testBase.Records[0].cf.request.uri = '/patients/branding_diabeloop_blue_favicon.ico'
+    const response = await handler(testBase, null)
+    expect(response).to.be.an('object')
     expect(response).to.be.deep.equal({
       status: 302,
-      statusDescription: "Found",
-      body: "",
+      statusDescription: 'Found',
+      body: '',
       headers: {
         location: [{
-          key: "Location",
-          value: "/branding_diabeloop_blue_favicon.ico"
-        }],
-      },
-    });
-  });
-});
+          key: 'Location',
+          value: '/branding_diabeloop_blue_favicon.ico'
+        }]
+      }
+    })
+  })
+})
