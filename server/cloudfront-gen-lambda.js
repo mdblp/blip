ba8852fd8d3b65b8ae42f31276097bf00617fd45
Yann Blanc <yann.blanc@diabeloop.fr>
2025-10-08 14:25:36 +0200

    fix lambda

--- server/cloudfront-gen-lambda.js
+++ server/cloudfront-gen-lambda.js
@@ -56,7 +56,7 @@ const contentSecurityPolicy = {
   baseUri: ["'none'"],
   formAction: ["'none'"],
   defaultSrc: ["'none'"],
-  scriptSrc: ["'strict-dynamic'", "'nonce-${nonce}'", "'unsafe-inline'", "'http:'", "'https:'"],
+  scriptSrc: ["'strict-dynamic'", "'nonce-${nonce}'", "'unsafe-inline'", "http:", "https:"],
   scriptSrcElem: ["'strict-dynamic'", "'nonce-${nonce}'"],
   styleSrc: ["'self'", "'unsafe-inline'"],
   imgSrc: ["'self'", 'data:'], // 'strict-dynamic' is problematic on google
