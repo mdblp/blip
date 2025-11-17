42ddf863c498c2ca3464ff1280b9c5612dd5fc5a
Célia <celia.rouis@diabeloop.fr>
2025-11-17 11:52:55 +0100

    PR review

--- packages/tideline/js/plot/eatingShortlyEvent.js
+++ packages/tideline/js/plot/eatingShortlyEvent.js
@@ -1,3 +1,20 @@
+/*
+ * == BSD2 LICENSE ==
+ * Copyright (c) 2014, Tidepool Project
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the associated License, which is identical to the BSD 2-Clause
+ * License as published by the Open Source Initiative at opensource.org.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the License for more details.
+ *
+ * You should have received a copy of the License along with this program; if
+ * not, you can obtain one from Tidepool Project at tidepool.org.
+ * == BSD2 LICENSE ==
+ */
+
 import _ from 'lodash'
 import * as d3 from 'd3'
 import eatingShortlyEventIcon from 'eating-shortly.svg'
@@ -16,6 +33,13 @@ function plotEatingShortlyEvent(pool, opts) {
   }
 
 
+  /**
+   * Renders eating shortly event icons on the plot.
+   * @param {d3.Selection} selection - The D3 selection to render the eating shortly events into.
+   * Uses opts.tidelineData.medicalData.eatingShortlyEvents for data.
+   * Adds SVG circles and images for each event, and sets up mouseover/mouseout handlers.
+   * @returns {void}
+   */
   function eatingShortlyEvent(selection) {
     var yPos = opts.r + opts.padding
     opts.xScale = pool.xScale().copy()
