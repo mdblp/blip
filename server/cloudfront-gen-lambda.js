252a43189baffecf0601d39128a0d0099868d851
Yann Blanc <yann.blanc@diabeloop.fr>
2026-02-16 15:02:33 +0100

    fix gen lambda

--- server/cloudfront-gen-lambda.js
+++ server/cloudfront-gen-lambda.js
@@ -389,10 +389,6 @@ switch (_.get(process, 'env.METRICS_SERVICE', 'disabled')) {
       indexHtml = indexHtml.replace(reMatomoJs, `$1${matomoConfigScripts}$3`)
 
       fs.writeFileSync(`${distDir}/static/${fileName}`, matomoJs)
-      fs.writeFileSync(`${distDir}/static/matoto`, matomoJs)
-      console.log('After write exists:', fs.existsSync(`${distDir}/static/${fileName}`));
-      console.log(fs.readFileSync(`${distDir}/static/${fileName}`, 'utf8').slice(0, 50));
-      console.log('  => Saved matomo config script hashed to : ' + distDir + '/static/' + fileName )
     } else {
       console.error('  /!\\ Invalid matomo config url, please verify your MATOMO_TRACKER_URL env variable /!\\')
     }
