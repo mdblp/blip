6648d7b3e3e6f3e934c65bb5c77228926600f4ab
Yann Blanc <yann.blanc@diabeloop.fr>
2026-02-17 11:35:53 +0100

    Revert "fix .0 in test"
    This reverts commit ddd9d1d3e28ee35237db4df7f89db4c80aa0f31a.

--- packages/dumb/src/components/stats/stats.util.ts
+++ packages/dumb/src/components/stats/stats.util.ts
@@ -33,10 +33,6 @@ export const ensureNumeric = (value: number | undefined | null): number => {
 
 
 export const formatNumberForLang = (decimal: number | string): string => {
-  /*For some cases, the value is set to --, so we keep this value for all languages */
-  if (decimal == '--') {
-    return '--'
-  }
   const lang = i18next.language
   const formatter = Intl.NumberFormat(lang)
   if (typeof decimal === "string") {
