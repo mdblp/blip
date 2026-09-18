801ec6abc10a8fa275ae2b3419ff1becd30ad260
Arthur Palma <arthur.palma@diabeloop.fr>
2026-09-18 11:01:02 +0200

    Updating integration tests

--- packages/yourloops/test/integration/assert/device-view-sections-overview.assert.ts
+++ packages/yourloops/test/integration/assert/device-view-sections-overview.assert.ts
@@ -36,28 +36,30 @@ export const checkDeviceSectionsOverviewVisible = (): void => {
 }
 
 export const checkDeviceSectionCardForCurrentParameters = (): void => {
-  expect(screen.getByText('System')).toBeVisible()
-  expect(screen.getByText('Pump')).toBeVisible()
-  expect(screen.getByText('CGM')).toBeVisible()
-  expect(screen.getByText('Total Daily Insulin')).toBeVisible()
-  expect(screen.getByText('Target glucose level')).toBeVisible()
-  expect(screen.getByText('Hypoglycemia threshold')).toBeVisible()
+  const currentSettingsSection = screen.getByTestId('device-view-overview-card-current-settings')
+  expect(currentSettingsSection).toBeVisible()
+  expect(currentSettingsSection).toHaveTextContent('System')
+  expect(currentSettingsSection).toHaveTextContent('Pump')
+  expect(currentSettingsSection).toHaveTextContent('CGM')
+  expect(currentSettingsSection).toHaveTextContent('Total Daily Insulin')
+  expect(currentSettingsSection).toHaveTextContent('Target glucose level')
+  expect(currentSettingsSection).toHaveTextContent('Hypoglycemia threshold')
 }
 
 export const checkDeviceSectionCardForSafetyBasal = (): void => {
-  expect(screen.getByText('In this section you can find the values of the patient safety basal profile.')).toBeVisible()
+  expect(screen.getByText('Values of the patient\'s safety basal profile.')).toBeVisible()
 }
 
 export const checkDeviceSectionCardForParametersHistory = async () => {
   const changeHistorySection = screen.getByTestId('device-view-overview-card-parameters-history')
   expect(changeHistorySection).toBeVisible()
-  expect(changeHistorySection).toHaveTextContent('Settings HistoryView moreLast update: 01/11/22 - 1:00 amAggressive...130 %90 % Aggressive...100 %110 % Aggressive...100 %90 % Hyperglyce...180.1 mg/dL140.0 mg/dL Hypoglycem...70.0 mg/dL60.0 mg/dL Aggressive...143 % Breakfast ...150.0 g Dinner - l...150.0 g Lunch - la...70.0 g Aggressive...100 % Aggressive...110 %100 % Aggressive...100 % Aggressive...90 %100 % Aggressive...130 % Breakfast ...70.0 g Dinner - a...60.0 g Lunch - av...50.0 g Aggressive...100 % Target glu...100.0 mg/dL Hyperglyce...180.1 mg/dL Hyperglyce...140.0 mg/dL180.1 mg/dL Hypoglycem...70.0 mg/dL Hypoglycem...60.0 mg/dL70.0 mg/dL Breakfast ...15.0 g Dinner - s...20.0 g Lunch - sm...30.0 g Total Dail...53.0 U Weight69.0 kg')
+  expect(changeHistorySection).toHaveTextContent('Settings HistoryView moreLast update: 01/11/22 - 1:00 amAggressiveness for lunch130 %90 %Aggressiveness for breakfast100 %110 %Aggressiveness for dinner100 %90 %Hyperglycemia threshold180.1 mg/dL140.0 mg/dLHypoglycemia threshold70.0 mg/dL60.0 mg/dLAggressiveness in hyperglycemia143 %Breakfast - large150.0 gDinner - large150.0 gLunch - large70.0 gAggressiveness for breakfast100 %Aggressiveness for breakfast110 %100 %Aggressiveness for dinner100 %Aggressiveness for dinner90 %100 %Aggressiveness for lunch130 %Breakfast - average70.0 gDinner - average60.0 gLunch - average50.0 gAggressiveness in normoglycemia100 %Target glucose level100.0 mg/dLHyperglycemia threshold180.1 mg/dLHyperglycemia threshold140.0 mg/dL180.1 mg/dLHypoglycemia threshold70.0 mg/dLHypoglycemia threshold60.0 mg/dL70.0 mg/dLBreakfast - small15.0 gDinner - small20.0 gLunch - small30.0 gTotal Daily Insulin53.0 UWeight69.0 kg')
 }
 
 export const checkDeviceSectionCardForDevicesHistory = async () => {
   const changeHistorySection = screen.getByTestId('device-view-overview-card-devices-history')
   expect(changeHistorySection).toBeVisible()
-  expect(changeHistorySection).toHaveTextContent('Devices HistoryView moreLast update: 01/11/22 - 1:00 amCGM manufacturerDexcom CGM productG5 HandsetMobiGo+ IMEI0123456789 Software version1.12.12-DBLG1-INS-DEXG6-COMMERCIAL Activation code123-456-789 Software version1.0.0 Pump manufacturerROCHE Pump productInsight Smartphone modelA212F')
+  expect(changeHistorySection).toHaveTextContent('Devices HistoryView moreLast update: 01/11/22 - 1:00 amCGM manufacturerDexcomCGM productG5HandsetMobiGo+IMEI0123456789Software version1.12.12-DBLG1-INS-DEXG6-COMMERCIALActivation code123-456-789Software version1.0.0Pump manufacturerROCHEPump productInsightSmartphone modelA212F')
 }
 
 export const checkClickViewMoreCurrentSettings = async () => {
