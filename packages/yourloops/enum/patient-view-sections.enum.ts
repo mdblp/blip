419fa523de2fe376d8c495f77c208e7b7ab34b1e
Arthur Palma <arthur.palma@diabeloop.fr>
2026-09-23 10:10:18 +0200

    deleting file

--- packages/yourloops/enum/patient-view-sections.enum.ts
+++
@@ -1,6 +0,0 @@
-export enum DeviceViewSectionsOverview {
-  CurrentSettings = 'current-settings',
-  BasalSafety = 'basal-safety-profile',
-  SettingsHistory = 'settings-change-history',
-  DevicesHistory = 'devices-change-history',
-}
